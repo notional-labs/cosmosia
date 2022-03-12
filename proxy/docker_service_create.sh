0fab47eef63bf32acc5f16a9dce00d91e9f32468
Tuan Pham Anh <baabeetaa@gmail.com>
2022-03-12 21:45:38 +0700

    testing nginx

---
+++ proxy/docker_service_create.sh
@@ -0,0 +1,13 @@
+# delete existing service
+docker service rm proxy
+
+
+docker service create \
+  --name proxy \
+  --replicas 1 \
+  --publish 80:80 \
+  --mount type=bind,source=$HOME/cosmosia,destination=/cosmosia \
+  --mount type=bind,source=$HOME/cosmosia/proxy/html:/usr/share/nginx/html:ro \
+  --mount type=bind,source=$HOME/cosmosia/proxy/nginx.conf:/etc/nginx/nginx.conf:ro \
+  --network cosmosia \
+  nginx:latest
