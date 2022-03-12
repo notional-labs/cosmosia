707ed05c1b2452f1390bbb4b3d01d4182a2e2816
Tuan Pham Anh <baabeetaa@gmail.com>
2022-03-12 21:48:04 +0700

    ...

--- proxy/docker_service_create.sh
+++ proxy/docker_service_create.sh
@@ -7,7 +7,7 @@ docker service create \
   --replicas 1 \
   --publish 80:80 \
   --mount type=bind,source=$HOME/cosmosia,destination=/cosmosia \
-  --mount type=bind,source=$HOME/cosmosia/proxy/html:/usr/share/nginx/html:ro \
-  --mount type=bind,source=$HOME/cosmosia/proxy/nginx.conf:/etc/nginx/nginx.conf:ro \
+  --mount type=bind,source=$HOME/cosmosia/proxy/html,destination=/usr/share/nginx/html:ro \
+  --mount type=bind,source=$HOME/cosmosia/proxy/nginx.conf,destination=/etc/nginx/nginx.conf:ro \
   --network cosmosia \
   nginx:latest
