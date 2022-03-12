ee6a252c8673c7a3d7209377c4bdddba69ac4ad2
Tuan Pham Anh <baabeetaa@gmail.com>
2022-03-12 16:23:13 +0700

    `cosmosia` overlay network

--- cosmoshub/docker_service_create.sh
+++ cosmoshub/docker_service_create.sh
@@ -8,5 +8,6 @@ docker service create \
   --publish 26658:26657 \
   --mount type=bind,source=$HOME/cosmosia,destination=/cosmosia \
   --mount type=bind,source=$HOME/cosmosia_data/cosmoshub,destination=/root \
+  --network cosmosia \
   archlinux:latest \
   /bin/bash /cosmosia/cosmoshub/quicksync.sh
