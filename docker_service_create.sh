1ac4b8d32fba98b326ced11215d304b318e59a87
Tuan Pham Anh <baabeetaa@gmail.com>
2022-03-14 03:02:12 +0700

    use one common docker_service_create.sh for all chain

---
+++ docker_service_create.sh
@@ -0,0 +1,23 @@
+# usage: ./docker_service_create.sh chain_name
+# eg., ./docker_service_create.sh cosmoshub
+
+chain_name="$1"
+
+if [[ -z $chain_name ]]
+then
+  echo "No chain_name. usage eg., ./docker_service_create.sh cosmoshub"
+fi
+
+
+# delete existing service
+docker service rm $chain_name
+
+
+docker service create \
+  --name $chain_name \
+  --replicas 1 \
+  --mount type=bind,source=$HOME/cosmosia,destination=/cosmosia,ro \
+  --mount type=bind,source=$HOME/cosmosia_data/$chain_name,destination=/root \
+  --network cosmosia \
+  archlinux:latest \
+  /bin/bash /cosmosia/quicksync.sh $chain_name
