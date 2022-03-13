c435fa2dbc54410a0634a33c9678112736a03714
Tuan Pham Anh <baabeetaa@gmail.com>
2022-03-14 00:02:05 +0700

    fix mount

--- cosmoshub/docker_service_create.sh
+++ cosmoshub/docker_service_create.sh
@@ -6,8 +6,8 @@ docker service create \
   --name cosmoshub \
   --replicas 1 \
   --publish 26658:26657 \
-  --mount type=bind,source=$HOME/cosmosia,destination=/cosmosia:ro \
+  --mount type=bind,source=$HOME/cosmosia,destination=/cosmosia,ro \
   --mount type=bind,source=$HOME/cosmosia_data/cosmoshub,destination=/root \
   --network cosmosia \
   archlinux:latest \
-  /bin/bash /cosmosia/cosmoshub/quicksync.sh
+  /bin/bash /cosmosia/quicksync.sh cosmoshub
