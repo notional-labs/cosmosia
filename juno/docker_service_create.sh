4bd9f02fde9cf91914103a5e79644e5443b2c4b3
Tuan Pham Anh <baabeetaa@gmail.com>
2022-03-14 02:20:51 +0700

    ...

--- juno/docker_service_create.sh
+++ juno/docker_service_create.sh
@@ -6,8 +6,8 @@ docker service create \
   --name juno \
   --replicas 1 \
   --publish 26657:26657 \
-  --mount type=bind,source=$HOME/cosmosia,destination=/cosmosia \
+  --mount type=bind,source=$HOME/cosmosia,destination=/cosmosia,ro \
   --mount type=bind,source=$HOME/cosmosia_data/juno,destination=/root \
   --network cosmosia \
   archlinux:latest \
-  /bin/bash /cosmosia/juno/quicksync.sh
+  /bin/bash /cosmosia/quicksync.sh juno
