76dc5974ff8c57555522b529eb21e1eae1dbcf0e
Tuan Pham Anh <baabeetaa@gmail.com>
2022-03-14 04:38:11 +0700

    store chain data on container

--- rpc/docker_service_create.sh
+++ rpc/docker_service_create.sh
@@ -12,14 +12,14 @@ fi
 # delete existing service
 docker service rm $chain_name
 
-
 docker service create \
   --name $chain_name \
   --replicas 1 \
-  --mount type=bind,source=$HOME/cosmosia,destination=/cosmosia,ro \
-  --mount type=bind,source=$HOME/cosmosia_data/$chain_name,destination=/root \
   --network cosmosia \
   archlinux:latest \
   /bin/bash -c \
   "curl -s https://raw.githubusercontent.com/baabeetaa/cosmosia/main/rpc/quicksync.sh > ~/quicksync.sh && \
   /bin/bash ~/quicksync.sh $chain_name"
+
+
+#  --mount type=bind,source=$HOME/cosmosia_data/$chain_name,destination=/root \
