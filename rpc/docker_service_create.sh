d573e1d8f112a6bf6169de224fc9109283afc3a1
Tuan Pham Anh <baabeetaa@gmail.com>
2022-03-14 05:01:06 +0700

    Fix cmd (#8)
    * fix docker cmd
    * store chain data on container

--- rpc/docker_service_create.sh
+++ rpc/docker_service_create.sh
@@ -6,18 +6,18 @@ chain_name="$1"
 if [[ -z $chain_name ]]
 then
   echo "No chain_name. usage eg., ./docker_service_create.sh cosmoshub"
+  exit
 fi
 
 
 # delete existing service
 docker service rm $chain_name
 
-
 docker service create \
   --name $chain_name \
   --replicas 1 \
-  --mount type=bind,source=$HOME/cosmosia,destination=/cosmosia,ro \
-  --mount type=bind,source=$HOME/cosmosia_data/$chain_name,destination=/root \
   --network cosmosia \
   archlinux:latest \
-  /bin/bash /cosmosia/quicksync.sh $chain_name
+  /bin/bash -c \
+  "curl -s https://raw.githubusercontent.com/baabeetaa/cosmosia/main/rpc/quicksync.sh > ~/quicksync.sh && \
+  /bin/bash ~/quicksync.sh $chain_name"
--- rpc/quicksync.sh
+++ rpc/quicksync.sh
@@ -6,12 +6,30 @@ chain_name="$1"
 if [[ -z $chain_name ]]
 then
   echo "No chain_name. usage eg., ./quicksynch.sh cosmoshub"
+  exit
 fi
+
+########################################################################################################################
+# prepare
+pacman -Syy --noconfirm go git base-devel wget jq
+
 
 ########################################################################################################################
 # read chain info
 # https://www.medo64.com/2018/12/extracting-single-ini-section-via-bash/
-source <(awk -v TARGET=$chain_name -F ' *= *' '
+
+#source <(awk -v TARGET=$chain_name -F ' *= *' '
+#  {
+#    if ($0 ~ /^\[.*\]$/) {
+#      gsub(/^\[|\]$/, "", $0)
+#      SECTION=$0
+#    } else if (($2 != "") && (SECTION==TARGET)) {
+#      print $1 "=" $2
+#    }
+#  }
+#  ' /cosmosia/data/chain_registry.ini)
+
+eval "$(curl -s https://raw.githubusercontent.com/baabeetaa/cosmosia/main/data/chain_registry.ini |awk -v TARGET=$chain_name -F ' *= *' '
   {
     if ($0 ~ /^\[.*\]$/) {
       gsub(/^\[|\]$/, "", $0)
@@ -20,7 +38,7 @@ source <(awk -v TARGET=$chain_name -F ' *= *' '
       print $1 "=" $2
     }
   }
-  ' /cosmosia/data/chain_registry.ini)
+  ')"
 
 # debug chain info
 echo "############################################################################################################"
@@ -37,13 +55,13 @@ echo "snapshot_provider=$snapshot_provider"
 if [[ -z $git_repo ]]
 then
   echo "Not support chain $chain_name"
+  exit
 fi
 
 
 ########################################################################################################################
 # build from source first
 echo "build from source:"
-pacman -Syy --noconfirm go git base-devel wget jq
 
 cd $HOME
 git clone $git_repo
