8116b9506c607342835b3e3c4958c7df21bcd7d9
Tuan Pham Anh <baabeetaa@gmail.com>
2022-03-31 02:20:43 +0700

    increase threshold to mark service unhealthy to 120s

--- rpc/healthcheck.sh
+++ rpc/healthcheck.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 BLOCKCHAIN_TIME=$(curl --silent --max-time 3 "http://localhost/status" |jq -r .result.sync_info.latest_block_time)
+THRESHOLD_TIME=120
 
 if [[ ${BLOCKCHAIN_TIME} == "null" ]]; then
   echo Status: 502
@@ -14,19 +15,19 @@ if [[ ! -z  "$BLOCKCHAIN_TIME" ]]; then
   BLOCKCHAIN_SECS=`date -d $BLOCKCHAIN_TIME +%s`
   CURRENT_SECS=`date +%s`
 
-  # if within 60 seconds of current time, call it synced and report healthy
+  # if within $THRESHOLD_TIME seconds of current time, call it synced and report healthy
   BLOCK_AGE=$((${CURRENT_SECS} - ${BLOCKCHAIN_SECS}))
 
-  if [[ ${BLOCK_AGE} -le 60 ]]; then
+  if [[ ${BLOCK_AGE} -le ${THRESHOLD_TIME} ]]; then
     echo Status: 200
     echo Content-type:text/plain
     echo
-    echo latest_block_time is less than 60 seconds, this node is considered healthy.
+    echo latest_block_time is less than $THRESHOLD_TIME seconds ago, this node is considered healthy.
   else
     echo Status: 503
     echo Content-type:text/plain
     echo
-    echo latest_block_time is higher than 60 seconds, this node is responding but not synced
+    echo latest_block_time is more than $THRESHOLD_TIME seconds ago, this node is responding but not synced
   fi
 else
   echo Status: 502
