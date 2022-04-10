9e894e2092b0454a3d71b645d0343073100c5632
Tuan Pham Anh <baabeetaa@gmail.com>
2022-04-10 16:38:49 +0700

    enable api for node...

--- rpc/quicksync.sh
+++ rpc/quicksync.sh
@@ -202,7 +202,7 @@ fi
 
 # set minimum gas prices & rpc port...
 sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"$minimum_gas_prices\"/" $node_home/config/app.toml
-sed -i '/^\[api]/,/^\[/{s/^enable[[:space:]]*=.*/enable = true/}' node_home/config/app.toml
+sed -i '/^\[api]/,/^\[/{s/^enable[[:space:]]*=.*/enable = true/}' $node_home/config/app.toml
 sed -i '/^\[rpc]/,/^\[/{s/^laddr[[:space:]]*=.*/laddr = "tcp:\/\/0.0.0.0:26657"/}' $node_home/config/config.toml
 sed -i -e "s/^max_num_inbound_peers *=.*/max_num_inbound_peers = 1000/" $node_home/config/config.toml
 sed -i -e "s/^max_num_outbound_peers *=.*/max_num_outbound_peers = 200/" $node_home/config/config.toml
