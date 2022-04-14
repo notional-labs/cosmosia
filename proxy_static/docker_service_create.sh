7af63d37d6fa614cc98e3306290eaa217d738b1b
Tuan Pham Anh <baabeetaa@gmail.com>
2022-04-14 09:05:33 +0700

    proxy static for status page, snapshots

---
+++ proxy_static/docker_service_create.sh
@@ -0,0 +1,21 @@
+# delete existing service
+docker service rm proxy_public
+
+# create new service
+docker service create \
+  --name proxy_public \
+  --replicas 1 \
+  --publish mode=host,target=80,published=80 \
+  --publish mode=host,target=443,published=443 \
+  --network cosmosia \
+  --constraint 'node.hostname==cosmosia2' \
+  --restart-condition any \
+  --restart-delay 3s \
+  --restart-max-attempts 3 \
+  --restart-window 10m \
+  --secret ssl_notional.ventures.tar.gz \
+  archlinux:latest \
+  /bin/bash -c \
+  "curl -s https://raw.githubusercontent.com/baabeetaa/cosmosia/main/proxy_public/run.sh > ~/run.sh && /bin/bash ~/run.sh"
+
+
---
+++ proxy_static/run.sh
@@ -0,0 +1,15 @@
+pacman -Syu --noconfirm
+pacman -S --noconfirm base-devel dnsutils python nginx logrotate
+
+# extract SSL cert (fullchain.pem and privkey.pem files)
+tar -xvf "/run/secrets/ssl_notional.ventures.tar.gz" -C /etc/nginx/
+
+########################################################################################################################
+# nginx
+
+curl -s https://raw.githubusercontent.com/baabeetaa/cosmosia/main/proxy_public/nginx.conf > /etc/nginx/nginx.conf
+
+/usr/sbin/nginx -g "daemon off;"
+
+# loop forever for debugging only
+#while true; do sleep 5; done
