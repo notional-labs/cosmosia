68d7ab3cb1663cc1d1a5081b5afb44c8dd9e81b0
Tuan Pham Anh <baabeetaa@gmail.com>
2022-04-14 09:17:10 +0700

    ...

--- proxy_static/run.sh
+++ proxy_static/run.sh
@@ -7,9 +7,9 @@ tar -xvf "/run/secrets/ssl_notional.ventures.tar.gz" -C /etc/nginx/
 ########################################################################################################################
 # nginx
 
-curl -s https://raw.githubusercontent.com/baabeetaa/cosmosia/main/proxy_public/nginx.conf > /etc/nginx/nginx.conf
+curl -s https://raw.githubusercontent.com/baabeetaa/cosmosia/main/proxy_static/nginx.conf > /etc/nginx/nginx.conf
 
-/usr/sbin/nginx -g "daemon off;"
+/usr/sbin/nginx -g "daemon off;"proxy_static
 
 # loop forever for debugging only
 #while true; do sleep 5; done
