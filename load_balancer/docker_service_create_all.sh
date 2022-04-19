6d69889f263d8ad3aa33cef1e1f32f714f39465c
Tuan Pham Anh <baabeetaa@gmail.com>
2022-04-20 04:06:05 +0700

    add script to deploy load-balancer for all chains

---
+++ load_balancer/docker_service_create_all.sh
@@ -0,0 +1,13 @@
+
+
+# deploy/re-deploy load-balancer for all chains
+
+
+RPC_SERVICES="osmosis starname regen akash cosmoshub sentinel emoney ixo juno sifchain likecoin kichain cyber cheqd stargaze bandchain chihuahua kava bitcanna konstellation omniflixhub terra vidulum provenance dig gravitybridge"
+
+
+for service_name in $RPC_SERVICES; do
+  /bin/bash docker_service_create.sh $service_name
+
+  sleep 60
+done
