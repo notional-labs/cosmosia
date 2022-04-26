2368c67a7a6a289105630f6c2b62074179bf1910
Tuan Pham Anh <baabeetaa@gmail.com>
2022-04-26 20:47:16 +0700

    add web_config

---
+++ web_config/docker_service_create.sh
@@ -0,0 +1,20 @@
+# delete existing service
+docker service rm web_config
+
+# create new service
+docker service create \
+  --name web_config \
+  --replicas 1 \
+  --network cosmosia \
+  --constraint 'node.role==manager' \
+  --endpoint-mode dnsrr \
+  --mount type=bind,source=/mnt/shared_storage/web_config,destination=/data/web_config \
+  --restart-condition any \
+  --restart-delay 3s \
+  --restart-max-attempts 3 \
+  --restart-window 10m \
+  archlinux:latest \
+  /bin/bash -c \
+  "curl -s https://raw.githubusercontent.com/baabeetaa/cosmosia/main/web_config/run.sh > ~/run.sh && /bin/bash ~/run.sh"
+
+
---
+++ web_config/run.sh
@@ -0,0 +1,12 @@
+pacman -Syu --noconfirm
+pacman -S --noconfirm nginx
+
+########################################################################################################################
+# nginx
+
+curl -s https://raw.githubusercontent.com/baabeetaa/cosmosia/main/web_config/nginx.conf > /etc/nginx/nginx.conf
+
+/usr/sbin/nginx -g "daemon off;"
+
+# loop forever for debugging only
+while true; do sleep 5; done
