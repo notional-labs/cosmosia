85d41fb9695fe4af99d522c2ba0ee4f78dcb1301
Tuan Pham Anh <baabeetaa@gmail.com>
2022-04-30 22:05:14 +0700

    ...

--- load_balancer/docker_service_create_caddy_monitor.sh
+++ load_balancer/docker_service_create_caddy_monitor.sh
@@ -6,6 +6,8 @@ docker service create \
   --name caddy_monitor_test \
   --replicas 1 \
   --publish mode=host,target=80,published=80 \
+  --publish mode=host,target=9090,published=9090 \
+  --publish mode=host,target=3000,published=3000 \
   --network cosmosia \
   --constraint 'node.hostname==cosmosia7' \
   --endpoint-mode dnsrr \
