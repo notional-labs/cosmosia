553db3a3d1eb7ed3508479a598365502ef795422
Tuan Pham Anh <baabeetaa@gmail.com>
2022-05-05 04:20:45 +0700

    remove netdata from proxy

--- proxy/run.sh
+++ proxy/run.sh
@@ -2,21 +2,6 @@ pacman -Syu --noconfirm
 pacman -S --noconfirm base-devel dnsutils python nginx logrotate
 
 ########################################################################################################################
-# netdata
-
-bash <(curl -Ss https://my-netdata.io/kickstart.sh)
-sleep 5
-
-killall netdata
-
-curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/main/proxy/netdata.conf > /opt/netdata/netdata-configs/netdata.conf
-curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/main/proxy/netdata.python.d.nginx.conf > /opt/netdata/netdata-configs/python.d/nginx.conf
-curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/main/proxy/netdata.python.d.web_log.conf > /opt/netdata/etc/netdata/python.d/web_log.conf
-
-/opt/netdata/bin/srv/netdata
-sleep 5
-
-########################################################################################################################
 # nginx
 
 curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/main/proxy/nginx.conf > /etc/nginx/nginx.conf
@@ -24,7 +9,6 @@ curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/main/proxy/inde
 
 #/usr/sbin/nginx -g "daemon off;"
 /usr/sbin/nginx
-sleep 5
 
 ########################################################################################################################
 # logrotate
@@ -33,14 +17,6 @@ sed -i -e "s/create.*/create 0644 root root/" /etc/logrotate.d/nginx
 
 ########################################################################################################################
 # big loop
-
-killall netdata
-killall nginx
-sleep 5
-/usr/sbin/nginx
-sleep 5
-/opt/netdata/bin/srv/netdata
-
 
 while true; do
   # need to use cron job for logrotate
