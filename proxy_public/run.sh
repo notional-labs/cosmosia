b29ffb5a317f14f08716acc30f7b5bd49cc65bf8
Tuan Pham Anh <baabeetaa@gmail.com>
2022-05-06 14:32:15 +0700

    - disable access log for nginx - disable logrotate for nginx

--- proxy_public/run.sh
+++ proxy_public/run.sh
@@ -11,27 +11,19 @@ curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/main/proxy_publ
 
 #/usr/sbin/nginx -g "daemon off;"
 /usr/sbin/nginx
-sleep 5
+#sleep 5
 
 ########################################################################################################################
-# logrotate
-sed -i -e "s/{.*/{\n\tdaily\n\trotate 2/" /etc/logrotate.d/nginx
-sed -i -e "s/create.*/create 0644 root root/" /etc/logrotate.d/nginx
+## logrotate
+#sed -i -e "s/{.*/{\n\tdaily\n\trotate 2/" /etc/logrotate.d/nginx
+#sed -i -e "s/create.*/create 0644 root root/" /etc/logrotate.d/nginx
 
 ########################################################################################################################
-# big loop
-
-##killall netdata
-#killall nginx
-#sleep 5
-#/usr/sbin/nginx
-#sleep 5
-##/opt/netdata/bin/srv/netdata
-
-while true; do
-   # need to use cron job for logrotate
-   logrotate /etc/logrotate.d/nginx
-
-  # sleep 1 day
-  sleep 86400
-done
+## big loop
+#while true; do
+#   # need to use cron job for logrotate
+#   logrotate /etc/logrotate.d/nginx
+#
+#  # sleep 1 day
+#  sleep 86400
+#done
