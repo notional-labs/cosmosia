1c1ef15d0ebac00d0fc7d88e415e2b8b496dd0d2
Tuan Pham Anh <baabeetaa@gmail.com>
2022-05-20 21:25:00 +0700

    add REST/API to private proxy

--- proxy_private/docker_service_create.sh
+++ proxy_private/docker_service_create.sh
@@ -42,6 +42,41 @@ docker service create \
   --publish mode=host,target=9033,published=9033 \
   --publish mode=host,target=9034,published=9034 \
   --publish mode=host,target=9035,published=9035 \
+  --publish mode=host,target=9001,published=9101 \
+  --publish mode=host,target=9002,published=9102 \
+  --publish mode=host,target=9003,published=9103 \
+  --publish mode=host,target=9004,published=9104 \
+  --publish mode=host,target=9005,published=9105 \
+  --publish mode=host,target=9006,published=9106 \
+  --publish mode=host,target=9007,published=9107 \
+  --publish mode=host,target=9008,published=9108 \
+  --publish mode=host,target=9009,published=9109 \
+  --publish mode=host,target=9010,published=9110 \
+  --publish mode=host,target=9011,published=9111 \
+  --publish mode=host,target=9012,published=9112 \
+  --publish mode=host,target=9013,published=9113 \
+  --publish mode=host,target=9014,published=9114 \
+  --publish mode=host,target=9015,published=9115 \
+  --publish mode=host,target=9016,published=9116 \
+  --publish mode=host,target=9017,published=9117 \
+  --publish mode=host,target=9018,published=9118 \
+  --publish mode=host,target=9019,published=9119 \
+  --publish mode=host,target=9020,published=9120 \
+  --publish mode=host,target=9021,published=9121 \
+  --publish mode=host,target=9022,published=9122 \
+  --publish mode=host,target=9023,published=9123 \
+  --publish mode=host,target=9024,published=9124 \
+  --publish mode=host,target=9025,published=9125 \
+  --publish mode=host,target=9026,published=9126 \
+  --publish mode=host,target=9027,published=9127 \
+  --publish mode=host,target=9028,published=9128 \
+  --publish mode=host,target=9029,published=9129 \
+  --publish mode=host,target=9030,published=9130 \
+  --publish mode=host,target=9031,published=9131 \
+  --publish mode=host,target=9032,published=9132 \
+  --publish mode=host,target=9033,published=9133 \
+  --publish mode=host,target=9034,published=9134 \
+  --publish mode=host,target=9035,published=9135 \
   --network cosmosia \
   --constraint 'node.hostname==cosmosia7' \
   --secret ssl_notional.ventures.tar.gz \
