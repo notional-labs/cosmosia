a2037c7487cf20169e0a5b3eadaadcb2f1c33e97
Tuan Pham Anh <baabeetaa@gmail.com>
2022-05-27 06:59:35 +0700

    Similar to https://github.com/notional-labs/cosmosia/issues/42 but for private proxy instead

--- proxy_private/run.sh
+++ proxy_private/run.sh
@@ -22,7 +22,33 @@ sleep 3
 tar -xvf "/run/secrets/ssl_notional.ventures.tar.gz" -C /etc/nginx/
 
 cp $HOME/cosmosia/proxy_private/nginx.conf /etc/nginx/nginx.conf
-cp $HOME/cosmosia/proxy_private/index.html /usr/share/nginx/html/index.html
+
+# generate index.html
+SERVICES=$(curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/main/data/chain_registry.ini |egrep -o "\[.*\]" | sed 's/^\[\(.*\)\]$/\1/')
+
+get_links () {
+  for service_name in $SERVICES; do
+    echo "<p><a href=\"/${service_name}/\">$service_name</a></p>"
+  done
+}
+
+links=$(get_links)
+
+cat <<EOT > /usr/share/nginx/html/index.html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Cosmosia</title>
+</head>
+
+<body>
+  <h3>RPC:</h3>
+  ${links}
+</body>
+</html>
+EOT
+
 
 #/usr/sbin/nginx -g "daemon off;"
 /usr/sbin/nginx
