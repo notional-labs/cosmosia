1047c55f36388ed187365bb5ba26b4a2b257ab29
Tuan Pham Anh <baabeetaa@gmail.com>
2022-06-01 10:19:38 +0700

    add 2 scripts for creating statesync snapshot manually.

---
+++ snapshot/scripts/create_snapshot.sh
@@ -0,0 +1,53 @@
+# usage: ./create_snapshot.sh chain_name
+# eg., ./snapshost_run.sh cosmoshub
+
+chain_name="$1"
+
+if [[ -z $chain_name ]]
+then
+  echo "No chain_name. usage eg., ./create_snapshot.sh cosmoshub"
+  exit
+fi
+
+
+eval "$(curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/main/data/chain_registry.ini |awk -v TARGET=$chain_name -F ' = ' '
+  {
+    if ($0 ~ /^\[.*\]$/) {
+      gsub(/^\[|\]$/, "", $0)
+      SECTION=$0
+    } else if (($2 != "") && (SECTION==TARGET)) {
+      print $1 "=" $2
+    }
+  }
+  ')"
+
+echo "node_home=$node_home"
+
+cd $node_home
+
+rm -rf $node_home/data/snapshots/*
+
+TAR_FILENAME="data_$(date +%Y%m%d_%T |sed 's/://g').tar.gz"
+TAR_FILE_PATH="$HOME/$TAR_FILENAME"
+
+# snapshot file includes ALL dirs in $node_home excluding config dir
+included_dirs=$(ls -d * |grep -v config| tr '\n' ' ')
+
+tar -czvf $TAR_FILE_PATH $included_dirs
+
+FILESIZE=$(stat -c%s "$TAR_FILE_PATH")
+
+# copy to /snapshot folder
+mv $TAR_FILE_PATH /snapshot/
+
+cat <<EOT > /snapshot/chain.json
+{
+    "snapshot_url": "./$chain_name/$TAR_FILENAME",
+    "file_size": $FILESIZE
+}
+EOT
+
+
+# delete old snapshots
+cd /snapshot/ && rm $(ls *.tar.gz |sort |head -n -1)
+
---
+++ snapshot/scripts/disable_statesync.sh
@@ -0,0 +1,29 @@
+# usage: ./disable_statesync.sh chain_name
+# eg., ./disable_statesync.sh cosmoshub
+
+chain_name="$1"
+
+if [[ -z $chain_name ]]
+then
+  echo "No chain_name. usage eg., ./disable_statesync.sh cosmoshub"
+  exit
+fi
+
+
+eval "$(curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/main/data/chain_registry.ini |awk -v TARGET=$chain_name -F ' = ' '
+  {
+    if ($0 ~ /^\[.*\]$/) {
+      gsub(/^\[|\]$/, "", $0)
+      SECTION=$0
+    } else if (($2 != "") && (SECTION==TARGET)) {
+      print $1 "=" $2
+    }
+  }
+  ')"
+
+echo "node_home=$node_home"
+
+sed -i -e "s/^snapshot-interval *=.*/snapshot-interval = 0/" $node_home/config/app.toml
+sed -i '/^\[statesync]/,/^\[/{s/^enable[[:space:]]*=.*/enable = false/}' $node_home/config/config.toml
+sed -i -e "s/^addr_book_strict *=.*/addr_book_strict = true/" $node_home/config/config.toml
+sed -i -e "s/^pex *=.*/pex = true/" $node_home/config/config.toml
