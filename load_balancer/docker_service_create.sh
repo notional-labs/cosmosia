a93f7e47b75fb01fb7e6e4e1821210e31b88f9bd
Tuan Pham Anh <baabeetaa@gmail.com>
2022-06-17 13:10:24 +0700

    ...

--- load_balancer/docker_service_create.sh
+++ load_balancer/docker_service_create.sh
@@ -1,7 +1,8 @@
-# usage: ./docker_service_create.sh chain_name
-# eg., ./docker_service_create.sh cosmoshub
+# usage: ./docker_service_create.sh chain_name [rpc_service_name]
+# eg., ./docker_service_create.sh cosmoshub [rpc_cosmoshub_3]
 
 chain_name="$1"
+rpc_service_name="$2"
 
 if [[ -z $chain_name ]]
 then
@@ -25,6 +26,6 @@ docker service create \
   archlinux:latest \
   /bin/bash -c \
   "curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/main/load_balancer/run.sh > ~/run.sh && \
-   /bin/bash ~/run.sh $chain_name"
+   /bin/bash ~/run.sh $chain_name $rpc_service_name"
 
 
