a6f294b0b820aeada387fea022c728b95e5e3bdf
Tuan Pham Anh <baabeetaa@gmail.com>
2022-08-11 04:11:35 +0700

    91 drop rocksdb (#92)
    * update README.md
    
    * updte chain_registry.ini
    
    * update public proxy
    
    * update statesync scripts
    
    * update snapshot html index

--- statesync/docker_service_create_statesync_snapshot.sh
+++ statesync/docker_service_create_statesync_snapshot.sh
@@ -1,18 +1,12 @@
-# usage: ./docker_service_create_statesync_snapshot.sh chain_name [db_backend]
-# eg., ./docker_service_create_statesync_snapshot.sh cosmoshub goleveldb
-# db_backend: goleveldb rocksdb, default is goleveldb
+# usage: ./docker_service_create_statesync_snapshot.sh chain_name
+# eg., ./docker_service_create_statesync_snapshot.sh cosmoshub
 
 chain_name="$1"
-db_backend="$2"
 
 if [[ -z $chain_name ]]
 then
-  echo "No chain_name. usage eg., ./docker_service_create_statesync_snapshot.sh cosmoshub [db_backend]"
+  echo "No chain_name. usage eg., ./docker_service_create_statesync_snapshot.sh cosmoshub"
   exit
-fi
-
-if [[ -z $db_backend ]]; then
-  db_backend="goleveldb"
 fi
 
 eval "$(awk -v TARGET=$chain_name -F ' = ' '
@@ -31,12 +25,6 @@ HOST="$snapshot_node"
 MOUNT_SRC="/mnt/data/snapshots/$chain_name"
 SERVICE_NAME="statesync_snapshot_$chain_name"
 
-if [[ $db_backend == "rocksdb" ]]; then
-  HOST="$rocksdb_snapshot_node"
-  SERVICE_NAME="rocksdb_statesync_snapshot_$chain_name"
-  MOUNT_SRC="/mnt/data/rocksdb_snapshots/$chain_name"
-fi
-
 echo "HOST=$HOST"
 echo "SERVICE_NAME=$SERVICE_NAME"
 
@@ -54,4 +42,4 @@ docker service create \
   archlinux:latest \
   /bin/bash -c \
   "curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/main/statesync/statesync_snapshot_run.sh > ~/statesync_snapshot_run.sh && \
-  /bin/bash ~/statesync_snapshot_run.sh $chain_name $db_backend"
+  /bin/bash ~/statesync_snapshot_run.sh $chain_name"
--- statesync/statesync_snapshot_run.sh
+++ statesync/statesync_snapshot_run.sh
@@ -1,17 +1,11 @@
-# usage: ./sss_run.sh chain_name [db_backend]
-# eg., ./sss_run.sh cosmoshub goleveldb
-# db_backend: goleveldb rocksdb, default is goleveldb
+# usage: ./sss_run.sh chain_name
+# eg., ./sss_run.sh cosmoshub
 
 chain_name="$1"
-db_backend="$2"
 
 if [[ -z $chain_name ]]; then
-  echo "No chain_name. usage eg., ./sss_run.sh cosmoshub [db_backend]"
+  echo "No chain_name. usage eg., ./sss_run.sh cosmoshub"
   exit
-fi
-
-if [[ -z $db_backend ]]; then
-  db_backend="goleveldb"
 fi
 
 # functions
@@ -47,7 +41,6 @@ echo "addrbook_url=$addrbook_url"
 echo "snapshot_provider=$snapshot_provider"
 echo "start_flags=$start_flags"
 echo "pacman_pkgs=$pacman_pkgs"
-echo "rocksdb_version=$rocksdb_version"
 
 if [[ -z $git_repo ]]; then
   echo "Not support chain $chain_name"
@@ -56,43 +49,6 @@ fi
 
 pacman -Syu --noconfirm
 pacman -Sy --noconfirm go git base-devel wget jq $pacman_pkgs
-
-if [[ $db_backend == "rocksdb" ]]; then
-  echo "#################################################################################################################"
-  echo "install rocksdb"
-
-  pacman -Sy --noconfirm cmake python snappy zlib bzip2 lz4 zstd
-
-  # ===============================================
-  # install gflags
-  cd $HOME
-  git clone https://github.com/gflags/gflags.git
-  cd gflags
-  mkdir build
-  cd build
-  cmake -DBUILD_SHARED_LIBS=1 -DGFLAGS_INSTALL_SHARED_LIBS=1 ..
-  make install
-
-
-  # ===============================================
-  # installing rocksdb from source
-  cd $HOME
-  git clone --single-branch --branch $rocksdb_version https://github.com/facebook/rocksdb
-  cd rocksdb
-  make -j4 install-shared
-  ldconfig
-
-  # ===============
-  cp --preserve=links /usr/local/lib/libgflags* /usr/lib/
-  cp --preserve=links /usr/local/lib/librocksdb.so* /usr/lib/
-  cp -r /usr/local/include/rocksdb /usr/include/rocksdb
-
-  # ===========
-  export CGO_CFLAGS="-I/usr/local/include"
-  export CGO_LDFLAGS="-L/usr/local/lib -lrocksdb -lstdc++ -lm -lz -lbz2 -lsnappy -llz4 -lzstd"
-fi
-
-
 
 echo "#################################################################################################################"
 echo "build chain from source:"
@@ -121,22 +77,11 @@ cd $repo_name
 # git checkout $version
 [[ $chain_name == "gravitybridge" ]] && cd module
 
-if [[ $db_backend == "rocksdb" ]]; then
-  if [ $( echo "${chain_name}" | egrep -c "^(regen|kava|evmos)$" ) -ne 0 ]; then
-    make install COSMOS_BUILD_OPTIONS=rocksdb TENDERMINT_BUILD_OPTIONS=rocksdb BUILD_TAGS=rocksdb
-  elif [[ $chain_name == "comdex" ]]; then
-    go install -tags rocksdb -ldflags "-w -s -X github.com/cosmos/cosmos-sdk/types.DBBackend=rocksdb" ./node
-    mv $HOME/go/bin/node $HOME/go/bin/$daemon_name
-  else
-    go install -tags rocksdb -ldflags "-w -s -X github.com/cosmos/cosmos-sdk/types.DBBackend=rocksdb" ./...
-  fi
+# fix axelar `make install` doesnt work
+if [[ $chain_name == "axelar" ]]; then
+  make build && mkdir -p $HOME/go/bin && cp ./bin/axelard $HOME/go/bin/
 else
-  # fix axelar `make install` doesnt work
-  if [[ $chain_name == "axelar" ]]; then
-    make build && mkdir -p $HOME/go/bin && cp ./bin/axelard $HOME/go/bin/
-  else
-    make install
-  fi
+  make install
 fi
 
 
@@ -155,7 +100,6 @@ echo "BLOCK_HEIGHT=$BLOCK_HEIGHT"
 echo "TRUST_HASH=$TRUST_HASH"
 echo "REMOTE_NODE_ID=$REMOTE_NODE_ID"
 echo "CHAIN_ID=$CHAIN_ID"
-
 
 # delete node home
 rm -rf $node_home/*
@@ -189,8 +133,6 @@ sed -i -e "s/^send_rate *=.*/send_rate = 51200000/" $node_home/config/config.tom
 sed -i -e "s/^recv_rate *=.*/recv_rate = 51200000/" $node_home/config/config.toml
 sed -i -e "s/^seeds *=.*/seeds = \"\"/" $node_home/config/config.toml
 sed -i -e "s/^pex *=.*/pex = false/" $node_home/config/config.toml
-###
-[[ $db_backend == "rocksdb" ]] && sed -i -e "s/^db_backend *=.*/db_backend = \"rocksdb\"/" $node_home/config/config.toml
 
 sleep 5
 
@@ -198,10 +140,6 @@ sleep 5
 mkdir -p $node_home/data/snapshots
 
 echo "start chain..."
-if [[ $db_backend == "rocksdb" ]]; then
-  $HOME/go/bin/$daemon_name start --db_backend=rocksdb  --p2p.persistent_peers=${REMOTE_NODE_ID}@tasks.sss_${chain_name}:26656
-else
-  $HOME/go/bin/$daemon_name start --p2p.persistent_peers=${REMOTE_NODE_ID}@tasks.sss_${chain_name}:26656
-fi
+$HOME/go/bin/$daemon_name start --p2p.persistent_peers=${REMOTE_NODE_ID}@tasks.sss_${chain_name}:26656
 
 loop_forever
