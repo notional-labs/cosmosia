843ddcda675ccb24f69285c54b77eefad0c0a7c7
Tuan Pham Anh <baabeetaa@gmail.com>
2022-09-10 04:08:27 +0700

    set HASURA_GRAPHQL_ADMIN_SECRET

--- indexer/hasura/docker_service_create.sh
+++ indexer/hasura/docker_service_create.sh
@@ -3,8 +3,7 @@
 
 chain_name="$1"
 
-if [[ -z $chain_name ]]
-then
+if [[ -z $chain_name ]]; then
   echo "No chain_name. usage eg., ./docker_service_create.sh juno"
   exit
 fi
@@ -13,6 +12,16 @@ SERVICE_NAME="hasura_${chain_name}"
 
 # delete existing service
 docker service rm $SERVICE_NAME
+
+
+# get hasura_graphql_admin_secret from docker swarm
+hasura_graphql_admin_secret=$(docker config inspect hasura_graphql_admin_secret.txt |jq -r '.[0].Spec.Data' |base64 --decode)
+
+if [[ -z $hasura_graphql_admin_secret ]]; then
+  echo "No hasura_graphql_admin_secret, Pls set password to docker config named hasura_graphql_admin_secret.txt and try again!"
+  exit
+fi
+
 
 docker service create \
   --name $SERVICE_NAME \
@@ -26,7 +35,7 @@ docker service create \
   --env HASURA_GRAPHQL_ENABLE_CONSOLE=true \
   --env HASURA_GRAPHQL_DEV_MODE=false \
   --env HASURA_GRAPHQL_ENABLED_LOG_TYPES=startup \
-  --env HASURA_GRAPHQL_ADMIN_SECRET=myadminsecretkey \
+  --env HASURA_GRAPHQL_ADMIN_SECRET=${hasura_graphql_admin_secret} \
   --env HASURA_GRAPHQL_UNAUTHORIZED_ROLE=anonymous \
   --env ACTION_BASE_URL=http://localhost:3000 \
   hasura/graphql-engine:v2.11.1
