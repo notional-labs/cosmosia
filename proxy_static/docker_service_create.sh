da3e8d8e3a10955aceac928e41d42bd6ede35d96
Tuan Pham Anh <baabeetaa@gmail.com>
2022-09-17 17:31:02 +0700

    move snapshot proxy out of public proxy (#177)

---
+++ proxy_static/docker_service_create.sh
@@ -0,0 +1,17 @@
+# delete existing service
+docker service rm proxy_static
+
+# create new service
+docker service create \
+  --name proxy_static \
+  --replicas 1 \
+  --publish mode=host,target=80,published=80 \
+  --publish mode=host,target=443,published=443 \
+  --network cosmosia \
+  --network snapshot \
+  --constraint 'node.hostname==cosmosia11' \
+  --sysctl 'net.ipv4.tcp_tw_reuse=1' \
+  --restart-condition none \
+  archlinux:latest \
+  /bin/bash -c \
+  "curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/main/proxy_static/run.sh > ~/run.sh && /bin/bash ~/run.sh"
---
+++ proxy_static/run.sh
@@ -0,0 +1,42 @@
+pacman -Syu --noconfirm
+pacman -S --noconfirm base-devel wget dnsutils nginx cronie screen
+
+########################################################################################################################
+# SSL for notional.ventures (fullchain.pem and privkey.pem files)
+wget "http://tasks.web_config/config/fullchain.pem" -O /etc/nginx/fullchain.pem
+wget "http://tasks.web_config/config/privkey.pem" -O /etc/nginx/privkey.pem
+
+########################################################################################################################
+# nginx
+curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/main/proxy_static/nginx.conf > /etc/nginx/nginx.conf
+
+# generate index.html
+SERVICES=$(curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/main/data/chain_registry.ini |egrep -o "\[.*\]" | sed 's/^\[\(.*\)\]$/\1/')
+
+get_links () {
+  for service_name in $SERVICES; do
+    echo "<p><a href=\"/${service_name}/\">$service_name</a></p>"
+  done
+}
+
+links=$(get_links)
+
+cat <<EOT > /usr/share/nginx/html/index.html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Cosmosia Snapshots</title>
+</head>
+
+<body>
+  <h3>Snapshots:</h3>
+  ${links}
+</body>
+</html>
+EOT
+
+########################################################################################################################
+echo "Done!"
+# loop forever for debugging only
+while true; do sleep 5; done
