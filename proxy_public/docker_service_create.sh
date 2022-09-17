8433ed9ca69b591e6edcdfb124fe8198d67dfa20
Tuan Pham Anh <baabeetaa@gmail.com>
2022-09-17 17:46:15 +0700

    for merge

--- proxy_public/docker_service_create.sh
+++ proxy_public/docker_service_create.sh
@@ -1,9 +1,9 @@
 # delete existing service
-docker service rm proxy_public_new
+docker service rm proxy_public
 
 # create new service
 docker service create \
-  --name proxy_public_new \
+  --name proxy_public \
   --replicas 1 \
   --publish mode=host,target=80,published=80 \
   --publish mode=host,target=443,published=443 \
@@ -14,4 +14,4 @@ docker service create \
   --restart-condition none \
   archlinux:latest \
   /bin/bash -c \
-  "curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/112-wildcard-dns-records/proxy_public/run.sh > ~/run.sh && /bin/bash ~/run.sh"
+  "curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/main/proxy_public/run.sh > ~/run.sh && /bin/bash ~/run.sh"
