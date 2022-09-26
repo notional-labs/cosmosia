e3af9c53ff225b2dfcf1cf01e3381236625119ba
Tuan Pham Anh <baabeetaa@gmail.com>
2022-09-27 03:37:25 +0700

    snapshot: enable compact, refactoring...

--- snapshot/snapshot_cronjob.sh
+++ snapshot/snapshot_cronjob.sh
@@ -14,47 +14,12 @@ get_next_version () {
   echo $ver
 }
 
-
 data_version=$(find_current_data_version)
-
-
 source $HOME/chain_info.sh
-
-echo "#################################################################################################################"
-echo "pruning..."
-
-echo "snapshot_prune=$snapshot_prune"
-
-if [[ $snapshot_prune == "cosmos-pruner" ]]; then
-  # check snapshot size large than threshold or not
-  chain_json_url="https://snapshot.notional.ventures/$chain_name/chain.json"
-
-  echo "start pruning..."
-
-  cd $node_home/data
-  echo "Before:"
-  du -h
-
-  # no need to compact, pebble will auto-compact after starting the chain again in few mins.
-  # Note that size after pruning is not smaller, however it'wll be compacted and smaller next time restarting
-  $HOME/go/bin/cosmos-pruner prune $node_home/data --app=$chain_name --backend=pebbledb --blocks=201600 --versions=362880 --compact=false
-
-  # Delete tx_index.db
-#    rm -rf $node_home/data/tx_index.db
-
-  echo "After prune:"
-  du -h
-
-  data_version=$(get_next_version)
-fi
-
-
-echo "#################################################################################################################"
-echo "creating snapshot file..."
-supervisorctl start chain
 
 ##############
 echo "wait till chain get synched..."
+supervisorctl start chain
 
 catching_up=true
 while [[ "$catching_up" != "false" ]]; do
@@ -71,19 +36,41 @@ supervisorctl stop chain
 sleep 60
 
 # need for osmosis only, will be removed in the future versions
-if [[ $chain_name == "osmosis" ]]; then
+if [ $( echo "${chain_name}" | egrep -c "^(osmosis|osmosis-testnet)$" ) -ne 0 ]; then
   supervisorctl start chain
   sleep 60
   supervisorctl stop chain
   sleep 60
 fi
 
+echo "#################################################################################################################"
+echo "pruning..."
+echo "snapshot_prune=$snapshot_prune"
 
-echo "creating snapshot..."
+if [[ $snapshot_prune == "cosmos-pruner" ]]; then
+  # check snapshot size large than threshold or not
+  chain_json_url="https://snapshot.notional.ventures/$chain_name/chain.json"
+
+  cd $node_home/data
+  echo "Before:"
+  du -h
+
+  # no need to compact, pebble will auto-compact after starting the chain again in few mins.
+  # Note that size after pruning is not smaller, however it'wll be compacted and smaller next time restarting
+  $HOME/go/bin/cosmos-pruner prune $node_home/data --app=$chain_name --backend=pebbledb --blocks=201600 --versions=362880 --compact=true
+
+  echo "After:"
+  du -h
+
+  data_version=$(get_next_version)
+fi
+
+echo "#################################################################################################################"
+echo "creating snapshot file..."
 cd $node_home
 
 TAR_FILENAME="data_$(date +%Y%m%d_%T |sed 's/://g').tar.gz"
-TAR_FILE_PATH="$HOME/$TAR_FILENAME"
+TAR_FILE_PATH="/snapshot/$TAR_FILENAME"
 
 # snapshot file includes ALL dirs in $node_home excluding config dir
 included_dirs=$(ls -d * |grep -v config| tr '\n' ' ')
@@ -93,7 +80,6 @@ tar -czvf $TAR_FILE_PATH $included_dirs
 FILESIZE=$(stat -c%s "$TAR_FILE_PATH")
 
 # copy to /snapshot folder
-mv $TAR_FILE_PATH /snapshot/
 cp $node_home/config/addrbook.json /snapshot/
 
 cat <<EOT > /snapshot/chain.json
