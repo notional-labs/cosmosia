f5487c32e65e48375e6423670a9b9d5c360808ad
Tuan Pham Anh <baabeetaa@gmail.com>
2022-10-10 21:54:49 +0700

    ...

--- monitor/rr-dns/cronjob.sh
+++ monitor/rr-dns/cronjob.sh
@@ -20,6 +20,11 @@ ips="${ips//,/ }"
 echo "domain_name=$domain_name"
 echo "ips=$ips"
 
+echo "get current dns_records..."
+current_dns_records=$(cat $HOME/dns_records.txt |jq -r .result[].content)
+echo "$current_dns_records"
+echo "--------"
+
 ###################################################################################################
 # functions
 
@@ -33,10 +38,72 @@ check_server_alive () {
   fi
 }
 
+check_server_in_current_dns_records () {
+  ip=$1
+  current_ip=$(cat $HOME/dns_records.txt |jq -r  --arg ip "$ip" '.result[]| select( .content == $ip ) |.content')
+  # echo "ip=$ip,current_ip=$current_ip"
+
+  if [ "$ip" == "$current_ip" ]; then
+    echo "yes"
+  else
+    echo "no"
+  fi
+}
+
+check_server_in_current_ip_list () {
+  server_ip=$1
+  for ip in $ips; do
+    if [ "$ip" == "$server_ip" ]; then
+      echo "yes"
+      return 0
+    fi
+  done
+
+  echo "no"
+}
+
 ###################################################################################################
 # main
 
+records_changed="no"
+
+echo "remove records not in the list of ips"
+for ip in $current_dns_records; do
+  ip_exist_in_list=$(check_server_in_current_ip_list $ip)
+  echo "ip_exist_in_list of $ip: $ip_exist_in_list"
+
+  if [ "$ip_exist_in_list" == "no" ]; then
+    echo "TODO: remove ip $ip"
+    records_changed="yes"
+  fi
+done
+echo "-----"
+
+#----------------------------------------------------
+echo "update dns records"
 for ip in $ips; do
   state=$(check_server_alive $ip)
   echo "state of $ip is $state"
+
+  ip_exist_in_records=$(check_server_in_current_dns_records $ip)
+  echo "ip_exist_in_records of $ip: $ip_exist_in_records"
+
+  if [ "$state" == "up" ]; then
+    if [ "$ip_exist_in_records" == "no" ]; then
+      echo "TODO: add ip $ip"
+      records_changed="yes"
+    fi
+  else
+    if [ "$ip_exist_in_records" == "yes" ]; then
+      echo "TODO: remove ip $ip"
+      records_changed="yes"
+    fi
+  fi
 done
+echo "-----"
+
+if [ "$records_changed" == "yes" ]; then
+  # get DNS records
+  source $HOME/cloudlfare_api_env.sh
+  curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE_ID/dns_records?type=A&name=test1.notional.ventures&match=all" -H "Content-Type:application/json" -H "X-Auth-Email:$CLOUDFLARE_X_AUTH_EMAIL" -H "X-Auth-Key:$CLOUDFLARE_X_AUTH_KEY" > $HOME/dns_records.txt
+fi
--- monitor/rr-dns/run.sh
+++ monitor/rr-dns/run.sh
@@ -1,5 +1,5 @@
 pacman -Syu --noconfirm
-pacman -S --noconfirm base-devel wget dnsutils nginx cronie screen
+pacman -S --noconfirm base-devel wget dnsutils nginx cronie screen jq
 
 cd $HOME
 
@@ -9,9 +9,9 @@ wget "http://tasks.web_config/config/cloudlfare_api_env.txt" -O $HOME/cloudlfare
 
 curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/29-use-cloudflare-api-to-automate-dns-config/monitor/rr-dns/cronjob.sh  > $HOME/cronjob.sh
 
-# get DNS record
+# get DNS records the first time
 source $HOME/cloudlfare_api_env.sh
-curl -X GET "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE_ID/dns_records?type=A&name=test1.notional.ventures&match=all" -H "Content-Type:application/json" -H "X-Auth-Email:$CLOUDFLARE_X_AUTH_EMAIL" -H "X-Auth-Key:$CLOUDFLARE_X_AUTH_KEY" > $HOME/dns_records.txt
+curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE_ID/dns_records?type=A&name=test1.notional.ventures&match=all" -H "Content-Type:application/json" -H "X-Auth-Email:$CLOUDFLARE_X_AUTH_EMAIL" -H "X-Auth-Key:$CLOUDFLARE_X_AUTH_KEY" > $HOME/dns_records.txt
 
 ########################################################################################################################
 echo "Done!"
