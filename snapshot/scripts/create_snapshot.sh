f7be05a91057b140ca6aa158c9190c4122a542c0
Tuan Pham Anh <baabeetaa@gmail.com>
2022-10-13 05:59:01 +0700

    faster gzip when creating tar.gz snapshot file

--- snapshot/scripts/create_snapshot.sh
+++ snapshot/scripts/create_snapshot.sh
@@ -33,7 +33,8 @@ TAR_FILE_PATH="$HOME/$TAR_FILENAME"
 # snapshot file includes ALL dirs in $node_home excluding config dir
 included_dirs=$(ls -d * |grep -v config| tr '\n' ' ')
 
-tar -czvf $TAR_FILE_PATH $included_dirs
+# tar -czvf $TAR_FILE_PATH $included_dirs
+tar -cvf - $included_dirs |gzip --fast > $TAR_FILE_PATH
 
 FILESIZE=$(stat -c%s "$TAR_FILE_PATH")
 
