7b79f8c8215b2c39173ac0e6eb0921547cc81294
Tuan Pham Anh <baabeetaa@gmail.com>
2022-11-06 16:56:20 +0700

    snapshot: proxy snapshots

---
+++ proxy_snapshot/docker_service_create.sh
@@ -0,0 +1,28 @@
+# usage: ./docker_service_create.sh swarm_node
+# eg., ./docker_service_create.sh cosmosia1
+
+swarm_node="$1"
+
+if [[ -z $swarm_node ]]
+then
+  echo "No swarm_node. usage eg., ./docker_service_create.sh cosmosia1"
+  exit
+fi
+
+SERVICE_NAME="proxysnapshot_$swarm_node"
+
+# delete existing service
+docker service rm $SERVICE_NAME
+
+# create new service
+docker service create \
+  --name $SERVICE_NAME \
+  --replicas 1 \
+  --publish mode=host,target=80,published=11111 \
+  --mount type=bind,source=/mnt/data/snapshots,destination=/snapshots,readonly \
+  --constraint "node.hostname==${swarm_node}" \
+  --restart-condition none \
+  archlinux:latest \
+  /bin/bash -c \
+  "curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/main/proxy_snapshot/run.sh > ~/run.sh && \
+  /bin/bash ~/run.sh"
