dd309d67e27fbd159193ee44e7b754b64b4ad09d
Tuan Pham Anh <baabeetaa@gmail.com>
2023-01-14 23:48:57 +0700

    mantlemint: add mantlemint service for evaluating https://github.com/notional-labs/cosmosia/issues/250

---
+++ mantlemint/docker_service_create.sh
@@ -0,0 +1,14 @@
+# delete existing service
+docker service rm mantlemint
+
+docker service create \
+  --name mantlemint \
+  --replicas 1 \
+  --network cosmosia \
+  --label 'cosmosia.service=mantlemint' \
+  --endpoint-mode dnsrr \
+  --sysctl 'net.ipv4.tcp_tw_reuse=1' \
+  --restart-condition none \
+  archlinux:latest \
+  /bin/bash -c \
+  "while true; do sleep 60; done"
