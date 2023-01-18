46f4d63628e486e5d12222f9eac55c3f896a4712
Tuan Pham Anh <baabeetaa@gmail.com>
2023-01-19 03:45:22 +0700

    mantlemint: move to cosmosia19 https://github.com/notional-labs/cosmosia/issues/250

--- mantlemint/docker_service_create.sh
+++ mantlemint/docker_service_create.sh
@@ -9,6 +9,7 @@ docker service create \
   --endpoint-mode dnsrr \
   --sysctl 'net.ipv4.tcp_tw_reuse=1' \
   --restart-condition none \
+  --constraint 'node.hostname==cosmosia19' \
   archlinux:latest \
   /bin/bash -c \
   "while true; do sleep 60; done"
