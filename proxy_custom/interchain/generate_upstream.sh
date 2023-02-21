b8b1178536a33ba9f977e319140821fe86ef2f48
Tuan Pham Anh <baabeetaa@gmail.com>
2023-02-22 06:24:16 +0700

    https://github.com/notional-labs/cosmosia/issues/270 (#272)

---
+++ proxy_custom/interchain/generate_upstream.sh
@@ -0,0 +1,52 @@
+SERVICES_JSONRPC="evmos evmos-testnet-archive evmos-archive"
+
+
+UPSTREAM_CONFIG_FILE="/etc/nginx/upstream.conf"
+UPSTREAM_CONFIG_FILE_TMP="/etc/nginx/upstream.conf.tmp"
+
+# generate upstream.conf
+echo "" > $UPSTREAM_CONFIG_FILE_TMP
+for service_name in $SERVICES; do
+  lb_ip=$(dig +short "tasks.lb_$service_name")
+  if [[ -z "$lb_ip" ]]; then
+    lb_ip="127.0.0.1"
+  fi
+  cat <<EOT >> $UPSTREAM_CONFIG_FILE_TMP
+    upstream backend_rpc_$service_name {
+        keepalive 16;
+        server $lb_ip:8000;
+    }
+
+    upstream backend_api_$service_name {
+        keepalive 16;
+        server $lb_ip:8001;
+    }
+
+    upstream backend_grpc_$service_name {
+        keepalive 16;
+        server $lb_ip:8003;
+    }
+EOT
+done
+
+sleep 1
+
+SERVICES_JSONRPC="evmos evmos-testnet-archive evmos-archive"
+
+for service_name in $SERVICES_JSONRPC; do
+  lb_ip=$(dig +short "tasks.lb_$service_name")
+    if [[ -z "$lb_ip" ]]; then
+      lb_ip="127.0.0.1"
+    fi
+    cat <<EOT >> $UPSTREAM_CONFIG_FILE_TMP
+      upstream backend_jsonrpc_$service_name {
+          keepalive 16;
+          server $lb_ip:8004;
+      }
+
+      upstream backend_wsjsonrpc_$service_name {
+          keepalive 16;
+          server $lb_ip:8005;
+      }
+EOT
+done
