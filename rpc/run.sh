4efac71b28573fd74e6796361ce9c58e551b8e13
Tuan Pham Anh <baabeetaa@gmail.com>
2023-05-02 03:43:22 +0700

    restart sei-testnet

--- rpc/run.sh
+++ rpc/run.sh
@@ -111,7 +111,7 @@ sleep 20
 EOT
 
 # need for evmos https://github.com/notional-labs/cosmosia/issues/192
-if [ $( echo "${chain_name}" | egrep -c "^(akash|bandchain|evmos|evmos-archive|evmos-testnet-archive|kava|provenance|persistent|quicksilver|regen|sei|terra|umee|kujira|stride|whitewhale|injective)$" ) -ne 0 ]; then
+if [ $( echo "${chain_name}" | egrep -c "^(akash|bandchain|evmos|evmos-archive|evmos-testnet-archive|kava|provenance|persistent|quicksilver|regen|sei|terra|umee|kujira|stride|whitewhale|injective|sei-testnet)$" ) -ne 0 ]; then
   echo "$random_minute */7 * * root /bin/bash $HOME/restart_cronjob.sh" > /etc/cron.d/cron_restart_chain
 
   crond
