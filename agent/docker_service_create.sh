347bf8158b4a548fcafd3a1319ae7142a8e1532f
Tuan Pham Anh <baabeetaa@gmail.com>
2023-05-14 06:47:23 +0700

    ...

--- agent/docker_service_create.sh
+++ agent/docker_service_create.sh
@@ -4,7 +4,6 @@ docker service rm agent
 # create new service
 docker service create \
   --name agent \
-  --replicas 1 \
   --network snapshot \
   --mode global \
   --constraint 'node.role==manager' \
