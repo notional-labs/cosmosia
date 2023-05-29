fad7f00d46d78d4076f0433089a8def31997c3d4
Tuan Pham Anh <baabeetaa@gmail.com>
2023-05-30 00:00:59 +0700

    add /snap_deploy

---
+++ admin/web/pages/api/snap_deploy.js
@@ -0,0 +1,22 @@
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "./auth/[...nextauth]";
+import { execute_bash } from '/helper/bash';
+
+export default async (req, res) => {
+  const session = await getServerSession(req, res, authOptions);
+
+  if (session === null) {
+    return res.send({error: "Access Denied."})
+  }
+
+  const body = req.body;
+  console.log('body: ', body);
+  const {chain} = body;
+
+  try {
+    const {stdout, stderr} = await execute_bash(`cd /root/cosmosia/rpc && sh docker_service_create.sh ${chain}`);
+    res.status(200).json({status: "success", data: stdout});
+  } catch ({error, stdout, stderr}) {
+    res.status(200).json({status: "error", message: error, data: stdout});
+  }
+}
