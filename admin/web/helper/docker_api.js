e4e9f77abe33481c5cbb6bb56eb87e540b36214e
Tuan Pham Anh <baabeetaa@gmail.com>
2023-05-29 09:10:52 +0700

    fix dockerApiServices

--- admin/web/helper/docker_api.js
+++ admin/web/helper/docker_api.js
@@ -22,7 +22,7 @@ const WEB_CONFIG_URL = "http://tasks.web_config:2375";
  * @param filter example: {"label":["cosmosia.service=rpc"]}
  */
 export const dockerApiServices = async (filter) => {
-  const url = `${WEB_CONFIG_URL}/services??filters=${encodeURIComponent(filter)}`;
+  const url = `${WEB_CONFIG_URL}/services?filters=${encodeURIComponent(filter)}`;
   console.log(`dockerApiServices: url=${url}`);
   const response = await fetch(url);
   const data = await response.json();
