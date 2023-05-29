68c9471832975997d174db1bb49da57c31948d8b
Tuan Pham Anh <baabeetaa@gmail.com>
2023-05-29 18:40:12 +0700

    ...

--- snapshot/docker_service_create.sh
+++ snapshot/docker_service_create.sh
@@ -1,5 +1,5 @@
-# usage: ./docker_service_create_snapshost.sh chain_name
-# eg., ./docker_service_create_snapshost.sh cosmoshub
+# usage: ./docker_service_create.sh chain_name
+# eg., ./docker_service_create.sh cosmoshub
 
 chain_name="$1"
 if [ -f "../env.sh" ]; then
@@ -10,7 +10,7 @@ else
 fi
 
 if [[ -z $chain_name ]]; then
-  echo "No chain_name. usage eg., ./docker_service_create_snapshost.sh cosmoshub"
+  echo "No chain_name. usage eg., ./docker_service_create.sh cosmoshub"
   exit
 fi
 
