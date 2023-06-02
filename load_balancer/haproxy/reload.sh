e5b813a03e04758418f649d6ce3519f0fef810e8
Tuan Pham Anh <baabeetaa@gmail.com>
2023-06-03 03:49:36 +0700

    fix https://github.com/notional-labs/cosmosia/issues/363 ...

--- load_balancer/haproxy/reload.sh
+++ load_balancer/haproxy/reload.sh
@@ -2,8 +2,6 @@ cd $HOME
 source ~/env.sh
 
 # generate config file, fix https://github.com/notional-labs/cosmosia/issues/363
-cat <<EOT >> /etc/haproxy/haproxy.cfg
-$(<$HOME/haproxy.cfg)
-EOT
+rpc_service_name="$rpc_service_name" envsubst < $HOME/haproxy.cfg > /etc/haproxy/haproxy.cfg
 
 haproxy -D -f /etc/haproxy/haproxy.cfg -sf $(cat /var/run/haproxy.pid)
