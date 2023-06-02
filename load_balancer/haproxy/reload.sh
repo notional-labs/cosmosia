98656b1e0df7acb366a8a78701464d66aef13886
Tuan Pham Anh <baabeetaa@gmail.com>
2023-06-02 18:20:59 +0700

    fix haproxy reload.sh

--- load_balancer/haproxy/reload.sh
+++ load_balancer/haproxy/reload.sh
@@ -1,3 +1,3 @@
 cd $HOME
 source ~/env.sh
-rpc_service_name=$rpc_service_name haproxy -D -f /etc/haproxy/haproxy.cfg -p /var/run/haproxy.pid -sf $(cat /var/run/haproxy.pid)
+rpc_service_name=$rpc_service_name haproxy -D -f /etc/haproxy/haproxy.cfg -sf $(cat /var/run/haproxy.pid)
