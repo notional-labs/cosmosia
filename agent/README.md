8becfb263fc67c9448025bbcff4d9065ffe6a41b
Tuan Pham Anh <baabeetaa@gmail.com>
2023-06-03 12:32:36 +0700

    agent: cgi-scripts for host resource usage https://github.com/notional-labs/cosmosia/issues/355

--- agent/README.md
+++ agent/README.md
@@ -1,4 +1,7 @@
 # Agent Service
 
-See https://github.com/notional-labs/cosmosia/issues/316
+Provide 2 functions:
+
+1. Help host to access docker swarm network (https://github.com/notional-labs/cosmosia/issues/316)
+2. Give basic info of host resource usage for web admin (https://github.com/notional-labs/cosmosia/issues/355)
 
---
+++ agent/host_resource_usage.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cpu_usage=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}')
+
+ram_total=$(free | grep Mem | awk '{print $2}')
+ram_usage=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
+
+disk_size=$(df --output=size -h / |sed 1d |sed 's/ //g')
+disk_usage=$(df --output=pcent / |sed 1d |sed 's/ //g')
+
+
+echo "Status: 502"
+echo "Content-type:application/json"
+echo ""
+echo "{"
+echo "    \"cpu_usage\":\"${cpu_usage}%\""
+echo "    \"ram_total\":\"${ram_total}%\""
+echo "    \"ram_usage\":\"${ram_usage}%\""
+echo "    \"disk_size\":\"${disk_size}\""
+echo "    \"disk_usage\":\"${disk_usage}\""
+echo "}"
