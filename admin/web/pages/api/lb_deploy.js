422eeec96e803632dedba33f55de9cbe1aa13b52
Tuan Pham Anh <baabeetaa@gmail.com>
2023-06-03 09:45:31 +0700

    add lb_type to /lb_deploy form https://github.com/notional-labs/cosmosia/issues/367

--- admin/web/pages/api/lb_deploy.js
+++ admin/web/pages/api/lb_deploy.js
@@ -11,13 +11,13 @@ export default async (req, res) => {
 
   const body = req.body;
   console.log('body: ', body);
-  const {chain, rpc_service} = body;
+  const {chain, rpc_service, lb_type} = body;
 
   try {
     const {
       stdout,
       stderr
-    } = await execute_bash(`cd /root/cosmosia/load_balancer && sh docker_service_create.sh ${chain} ${rpc_service}`);
+    } = await execute_bash(`cd /root/cosmosia/load_balancer && sh docker_service_create.sh ${chain} ${rpc_service} ${lb_type}`);
     res.status(200).json({status: "success", data: stdout});
   } catch ({error, stdout, stderr}) {
     res.status(200).json({status: "error", message: error, data: stdout});
