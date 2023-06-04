f5284dbccec32132f450becec4132176a4115ad8
Tuan Pham Anh <baabeetaa@gmail.com>
2023-06-05 06:27:20 +0700

    admin: display date as time ago (#377)

--- admin/web/helper/utils.js
+++ admin/web/helper/utils.js
@@ -1,4 +1,22 @@
+import TimeAgo from 'javascript-time-ago';
+
+// English.
+import en from 'javascript-time-ago/locale/en'
+
+TimeAgo.addDefaultLocale(en)
+// Create formatter (English).
+const timeAgo = new TimeAgo('en-US');
+
 
 export const format2Decimal = (num) => {
   return (Math.round(num * 100) / 100).toFixed(2);
+}
+
+export const timeAgoFormat = (dt) => {
+  if (typeof dt === 'string' || dt instanceof String) {
+    const d = Date.parse(dt);
+    return timeAgo.format(d, 'mini');
+  }
+
+  return timeAgo.format(dt, 'mini');
 }
