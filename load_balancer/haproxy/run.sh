fc61f6c04acc0e903e66268befca134f9edd00af
Tuan Pham Anh <baabeetaa@gmail.com>
2023-06-04 07:32:08 +0700

    lb: add eth-jsonrpc and eth-jsonrpcws for needed chains https://github.com/notional-labs/cosmosia/issues/373

--- load_balancer/haproxy/run.sh
+++ load_balancer/haproxy/run.sh
@@ -15,7 +15,6 @@ pacman -S --noconfirm base-devel jq dnsutils python haproxy screen wget cronie
 
 # write env vars to bash file, so that cronjobs or other scripts could know
 cat <<EOT >> $HOME/env.sh
-CHAIN_REGISTRY_INI_URL="$CHAIN_REGISTRY_INI_URL"
 rpc_service_name="$rpc_service_name"
 EOT
 
@@ -25,6 +24,13 @@ source $HOME/env.sh
 # haproxy
 
 curl -Ls "https://raw.githubusercontent.com/notional-labs/cosmosia/main/load_balancer/haproxy/haproxy.cfg" > $HOME/haproxy.cfg
+
+# enable eth for needed chains by appending haproxy.eth.cfg to haproxy.cfg
+if [[ $rpc_service_name == rpc_evmos* ]]; then
+  curl -Ls "https://raw.githubusercontent.com/notional-labs/cosmosia/main/load_balancer/haproxy/haproxy.eth.cfg" >> $HOME/haproxy.cfg
+fi
+
+
 curl -Ls "https://raw.githubusercontent.com/notional-labs/cosmosia/main/load_balancer/haproxy/start.sh" > $HOME/start.sh
 curl -Ls "https://raw.githubusercontent.com/notional-labs/cosmosia/main/load_balancer/haproxy/reload.sh" > $HOME/reload.sh
 
