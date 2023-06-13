fe1df242bc53e2fe668d8355ea5393aea2f966e3
Tuan Pham Anh <baabeetaa@gmail.com>
2023-06-14 00:21:06 +0700

    cfg: add USE_SNAPSHOT_PROXY_URL to env.sh - empty means use internal proxy - otherwise use public proxy, it could be the same cluster or not (in case someone wants to deploy Cosmosia cluster without snapshot service, they are interested in running rpc services only)

--- env.sample.sh
+++ env.sample.sh
@@ -1,4 +1,5 @@
 CHAIN_REGISTRY_INI_URL=https://raw.githubusercontent.com/notional-labs/cosmosia/main/data/chain_registry.ini
+USE_SNAPSHOT_PROXY_URL=https://snapshot.notional.ventures/
 COSMOSIA_ADMIN_NEXTAUTH_URL=https://admin.cosmosia.notional.ventures
 COSMOSIA_ADMIN_NEXTAUTH_SECRET=secret
 COSMOSIA_ADMIN_PASSWORD=admin
