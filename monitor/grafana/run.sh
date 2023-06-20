278b12c56549b1f45c0ea5f6a38016bad2b71ad9
Tuan Pham Anh <baabeetaa@gmail.com>
2023-06-20 21:34:14 +0700

    grafana: use GRAFANA_PASSWORD docker config

--- monitor/grafana/run.sh
+++ monitor/grafana/run.sh
@@ -5,7 +5,7 @@ cd $HOME
 
 ############
 # prometheus
-echo "Intalling prometheus..."
+echo "Installing prometheus..."
 pacman -S --noconfirm prometheus
 
 # generate prometheus.yaml
@@ -41,8 +41,10 @@ curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/main/monitor/gr
 curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/main/monitor/grafana/MyDashboard.json > /var/lib/grafana/dashboards/MyDashboard.json
 
 # change admin password
-default_password="$(cat /run/secrets/grafana_password)"
-sed -i -e "s/^admin_password *=.*/admin_password = $default_password/" /usr/share/grafana/conf/defaults.ini
+default_password="$(curl -s "http://tasks.web_config/config/GRAFANA_PASSWORD")"
+if [[ $default_password != "NOT_FOUND" ]]; then
+  sed -i -e "s/^admin_password *=.*/admin_password = $default_password/" /usr/share/grafana/conf/defaults.ini
+fi
 
 screen -S grafana -dm /usr/sbin/grafana-server -homepath /usr/share/grafana
 
