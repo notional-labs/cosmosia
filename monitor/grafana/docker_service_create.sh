05b657c9a5e2def46029ecd08d6fa4f660e2ed27
Tuan Pham Anh <baabeetaa@gmail.com>
2023-06-20 21:49:37 +0700

    grafana: still need env.sh, lol

--- monitor/grafana/docker_service_create.sh
+++ monitor/grafana/docker_service_create.sh
@@ -1,6 +1,13 @@
 # Note:
 # set grafana admin default password by using docker swarm config with key GRAFANA_PASSWORD
 
+if [ -f "../../env.sh" ]; then
+  source ../../env.sh
+else
+    echo "../../env.sh file does not exist."
+    exit
+fi
+
 # delete existing service
 docker service rm grafana
 
@@ -11,6 +18,7 @@ docker service create \
   --network cosmosia \
   --endpoint-mode dnsrr \
   --restart-condition any \
+  --env-file ../../env.sh \
   archlinux:latest \
   /bin/bash -c \
   "curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/main/monitor/grafana/run.sh > ~/run.sh && /bin/bash ~/run.sh"
