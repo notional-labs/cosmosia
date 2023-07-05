00b84a0bafe0a73c916172311de1dd9965606a2a
Tuan Pham Anh <baabeetaa@gmail.com>
2023-07-05 22:33:04 +0700

    rpc: fix healthcheck.sh for sei

--- rpc/healthcheck.sh
+++ rpc/healthcheck.sh
@@ -20,7 +20,7 @@ if [[ $chain_name == *-sub* ]] && [[ $chain_name != *-sub ]]; then
 fi
 
 BLOCKCHAIN_TIME=$(curl --silent --max-time 3 "http://localhost/status" |jq -r .result.sync_info.latest_block_time)
-if [[ $chain_name == "sei-testnet" ]]; then
+if [ $( echo "${chain_name}" | egrep -c "^(sei|sei-testnet)$" ) -ne 0 ]; then
   BLOCKCHAIN_TIME=$(curl --silent --max-time 3 "http://localhost/status" |jq -r .sync_info.latest_block_time)
 fi
 
