70611a199d0496797dd4ed09ea61beb8138eedf3
Tuan Pham Anh <baabeetaa@gmail.com>
2023-08-07 15:37:24 +0700

    notionalapi: add rqlite db

---
+++ notionalapi/db/docker_service_create.sh
@@ -0,0 +1,17 @@
+# usage: ./docker_service_create.sh
+# eg., ./docker_service_create.sh
+
+SERVICE_NAME="napidb"
+
+# delete existing service
+docker service rm $SERVICE_NAME
+
+docker service create \
+  --name $SERVICE_NAME \
+  --replicas 1
+  --network notionalapi \
+  --label 'cosmosia.service=napidb' \
+  --constraint node.labels.notionalapi==true \
+  --endpoint-mode dnsrr \
+  --restart-condition none \
+  rqlite/rqlite -on-disk=true
