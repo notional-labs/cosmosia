eec843b52923e687135f09e7b9a20c3948d53408
Tuan Pham Anh <baabeetaa@gmail.com>
2023-08-07 17:45:09 +0700

    notionalapi: web (#417)
    * notionalapi: add web

---
+++ notionalapi/web/docker_service_create.sh
@@ -0,0 +1,20 @@
+
+SERVICE_NAME="napiweb"
+
+# delete existing service
+docker service rm $SERVICE_NAME
+
+# create new service
+docker service create \
+  --name $SERVICE_NAME \
+  --replicas 1 \
+  --network notionalapi \
+  --network cosmosia \
+  --constraint "node.labels.cosmosia.notionalapi==true" \
+  --endpoint-mode dnsrr \
+  --restart-condition any \
+  archlinux:latest \
+  /bin/bash -c \
+  "curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/main/notionalapi/web/run.sh > ~/run.sh && /bin/bash ~/run.sh"
+
+
