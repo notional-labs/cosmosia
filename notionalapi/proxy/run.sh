2da0f422d962273485d55058ec7cf76d38cd5611
Tuan Pham Anh <baabeetaa@gmail.com>
2023-08-07 23:26:17 +0700

    notionalapi: add proxy

---
+++ notionalapi/proxy/run.sh
@@ -0,0 +1,97 @@
+pacman -Syu --noconfirm
+pacman -S --noconfirm base-devel wget dnsutils nginx cronie screen
+
+# write env vars to bash file, so that cronjobs or other scripts could know
+cat <<EOT >> $HOME/env.sh
+CHAIN_REGISTRY_INI_URL="$CHAIN_REGISTRY_INI_URL"
+EOT
+
+########################################################################################################################
+# SSL (fullchain.pem and privkey.pem files)
+wget "http://tasks.web_config/config/notionalapi.com_fullchain.pem" -O /etc/nginx/fullchain.pem
+wget "http://tasks.web_config/config/notionalapi.com_privkey.pem" -O /etc/nginx/privkey.pem
+
+########################################################################################################################
+# nginx
+
+curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/main/proxy/public/nginx.conf > $HOME/nginx.conf.template
+cat $HOME/nginx.conf.template |envsubst '$USE_DOMAIN_NAME' > /etc/nginx/nginx.conf
+
+# generate index.html
+SERVICES=$(curl -s "$CHAIN_REGISTRY_INI_URL" |grep -E "\[.*\]" | sed 's/^\[\(.*\)\]$/\1/')
+
+get_links () {
+  for service_name in $SERVICES; do
+    echo "<p><a href=\"/${service_name}/\">$service_name</a></p>"
+  done
+}
+
+links=$(get_links)
+
+cat <<EOT > /usr/share/nginx/html/index.html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Cosmosia Snapshots</title>
+</head>
+
+<body>
+  <h3>Snapshots:</h3>
+  ${links}
+</body>
+</html>
+EOT
+
+########################################################################################################################
+
+# generate config for the first time
+curl -Ls "https://raw.githubusercontent.com/notional-labs/cosmosia/main/proxy/legacy/generate_upstream.sh" > $HOME/generate_upstream.sh
+sleep 1
+source $HOME/generate_upstream.sh
+echo "UPSTREAM_CONFIG_FILE=$UPSTREAM_CONFIG_FILE"
+echo "UPSTREAM_CONFIG_FILE_TMP=$UPSTREAM_CONFIG_FILE_TMP"
+sleep 1
+cat "$UPSTREAM_CONFIG_FILE_TMP" > "$UPSTREAM_CONFIG_FILE"
+sleep 1
+
+# run nginx with screen to avoid log to docker
+screen -S nginx -dm /usr/sbin/nginx -g "daemon off;"
+
+########################################################################################################################
+# cron
+cat <<'EOT' >  $HOME/cron_update_upstream.sh
+source $HOME/env.sh
+source $HOME/generate_upstream.sh
+sleep 1
+
+if cmp -s "$UPSTREAM_CONFIG_FILE" "$UPSTREAM_CONFIG_FILE_TMP"; then
+  # the same => do nothing
+  echo "no config change, do nothing..."
+else
+  # different
+
+  # show the diff
+  diff -c "$UPSTREAM_CONFIG_FILE" "$UPSTREAM_CONFIG_FILE_TMP"
+
+  echo "found config changes, updating..."
+  cat "$UPSTREAM_CONFIG_FILE_TMP" > "$UPSTREAM_CONFIG_FILE"
+  sleep 1
+  /usr/sbin/nginx -s reload
+fi
+EOT
+
+sleep 1
+echo "*/5 * * * * root /bin/bash $HOME/cron_update_upstream.sh" > /etc/cron.d/cron_update_upstream
+sleep 1
+crond
+
+########################################################################################################################
+## logrotate
+#sed -i -e "s/{.*/{\n\tdaily\n\trotate 2/" /etc/logrotate.d/nginx
+#sed -i -e "s/create.*/create 0644 root root/" /etc/logrotate.d/nginx
+
+########################################################################################################################
+echo "Done!"
+# loop forever for debugging only
+while true; do sleep 5; done
