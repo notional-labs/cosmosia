ab73f7e72f06811acf4613a1a5a2e9bd57842376
Tuan Pham Anh <baabeetaa@gmail.com>
2023-08-07 18:48:19 +0700

    notionalapi: add gw

---
+++ notionalapi/gateway/run.sh
@@ -0,0 +1,45 @@
+# usage: ./run.sh chain_name
+# eg., ./run.sh cosmoshub
+
+chain_name="$1"
+
+if [[ -z $chain_name ]]; then
+  echo "No chain_name. usage eg., ./run.sh cosmoshub"
+  exit
+fi
+
+# functions
+loop_forever () {
+  echo "loop forever for debugging only"
+  while true; do sleep 5; done
+}
+
+pacman -Syu --noconfirm
+pacman -Sy --noconfirm go git screen
+
+echo "#################################################################################################################"
+echo "install gateway"
+
+cd $HOME
+git clone --single-branch --branch main https://github.com/notional-labs/notionalapi
+cd notionalapi/gateway
+make install
+
+########################################################################################################################
+# config file
+cat <<EOT > $HOME/gateway.yaml
+aggrurl: "http://tasks.napiaggregator:8300/metering"
+dbconurl: "http://tasks.napidb:4001/"
+rpc: "http://tasks.lb_${chain_name}:8000"
+api: "http://tasks.lb_${chain_name}:8001"
+grpc: "tasks.lb_${chain_name}:8003"
+eth: "http://tasks.lb_${chain_name}:8004"
+ethws: "ws://tasks.lb_${chain_name}:8005"
+EOT
+
+########################################################################################################################
+# run
+cd $HOME
+screen -S gateway -dm /root/go/bin/gateway start --conf=/root/gateway.yaml
+
+loop_forever
