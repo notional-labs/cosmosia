6a4b337829c94356f572ac24e2f294ef84e378a8
Tuan Pham Anh <baabeetaa@gmail.com>
2023-08-09 06:32:00 +0700

    napi/gw: script to redeploy all

---
+++ notionalapi/gateway/redeploy_all.sh
@@ -0,0 +1,63 @@
+SERVICES=$(cat <<-END
+akash
+assetmantle
+axelar
+bitcanna
+bitsong
+celestia-testnet
+cheqd
+chihuahua
+composable
+cosmoshub-archive-sub1
+cosmoshub-archive-sub
+cosmoshub
+cryptoorgchain
+cyber
+dig-archive
+dig
+emoney
+evmos-archive-sub1
+evmos-archive-sub2
+evmos-archive-sub3
+evmos-archive-sub
+evmos
+fetchhub
+gravitybridge
+injective
+irisnet
+ixo
+juno
+kava
+kichain
+konstellation
+kujira
+mars
+noble
+omniflixhub
+osmosis-testnet
+osmosis
+persistent
+quasar
+quicksilver
+regen
+sei-archive-sub
+sei
+sentinel
+stargaze
+starname
+stride
+terra2
+terra
+umee
+vidulum
+whitewhale-testnet
+whitewhale
+END
+)
+
+for service_name in $SERVICES; do
+  echo "redeploying $service_name"
+  sh docker_service_create.sh $service_name
+  sleep 3
+done
+
