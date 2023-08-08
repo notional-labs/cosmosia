ecd23fe4113e21cb165bc1d1faaccdb9c4eaf088
Tuan Pham Anh <baabeetaa@gmail.com>
2023-08-08 14:33:04 +0700

    notionalapi: add proxy (#420)
    * notionalapi: add proxy

---
+++ notionalapi/proxy/docker_service_create.sh
@@ -0,0 +1,19 @@
+# delete existing service
+docker service rm napi_proxy
+
+# create new service
+docker service create \
+  --name napi_proxy \
+  --replicas 1 \
+  --publish mode=host,target=80,published=80 \
+  --publish mode=host,target=443,published=443 \
+  --publish mode=host,target=9090,published=9090 \
+  --network cosmosia \
+  --network notionalapi \
+  --constraint 'node.hostname==cosmosia31' \
+  --sysctl 'net.ipv4.tcp_tw_reuse=1' \
+  --restart-condition none \
+  --env-file ../../env.sh \
+  archlinux:latest \
+  /bin/bash -c \
+  "curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/main/notionalapi/proxy/run.sh > ~/run.sh && /bin/bash ~/run.sh"
---
+++ notionalapi/proxy/run.sh
@@ -0,0 +1,70 @@
+pacman -Syu --noconfirm
+pacman -S --noconfirm base-devel wget dnsutils nginx cronie screen
+
+# write env vars to bash file, so that cronjobs or other scripts could know
+cat <<EOT > $HOME/env.sh
+CHAIN_REGISTRY_INI_URL="$CHAIN_REGISTRY_INI_URL"
+EOT
+
+########################################################################################################################
+# SSL (fullchain.pem and privkey.pem files)
+wget "http://tasks.web_config/config/notionalapi.com_fullchain.pem" -O /etc/nginx/fullchain.pem
+wget "http://tasks.web_config/config/notionalapi.com_privkey.pem" -O /etc/nginx/privkey.pem
+
+########################################################################################################################
+# nginx
+
+curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/main/notionalapi/proxy/nginx.conf > /etc/nginx/nginx.conf
+
+########################################################################################################################
+
+# generate config for the first time
+curl -Ls "https://raw.githubusercontent.com/notional-labs/cosmosia/main/notionalapi/proxy/generate_upstream.sh" > $HOME/generate_upstream.sh
+sleep 1
+source $HOME/generate_upstream.sh
+echo "UPSTREAM_CONFIG_FILE=$UPSTREAM_CONFIG_FILE"
+echo "UPSTREAM_CONFIG_FILE_TMP=$UPSTREAM_CONFIG_FILE_TMP"
+sleep 1
+cat "$UPSTREAM_CONFIG_FILE_TMP" > "$UPSTREAM_CONFIG_FILE"
+sleep 1
+
+# run nginx with screen to avoid log to docker
+screen -S nginx -dm /usr/sbin/nginx -g "daemon off;"
+
+########################################################################################################################
+# cron
+cat <<'EOT' >  $HOME/cron_update_upstream.sh
+source $HOME/env.sh
+source $HOME/generate_upstream.sh
+sleep 1
+
+if cmp -s "$UPSTREAM_CONFIG_FILE" "$UPSTREAM_CONFIG_FILE_TMP"; then
+  # the same => do nothing
+  echo "no config change, do nothing..."
+else
+  # different
+
+  # show the diff
+  diff -c "$UPSTREAM_CONFIG_FILE" "$UPSTREAM_CONFIG_FILE_TMP"
+
+  echo "found config changes, updating..."
+  cat "$UPSTREAM_CONFIG_FILE_TMP" > "$UPSTREAM_CONFIG_FILE"
+  sleep 1
+  /usr/sbin/nginx -s reload
+fi
+EOT
+
+sleep 1
+echo "*/5 * * * * root /bin/bash $HOME/cron_update_upstream.sh" > /etc/cron.d/cron_update_upstream
+sleep 1
+crond
+
+########################################################################################################################
+## logrotate
+#sed -i -e "s/{.*/{\n\tdaily\n\trotate 2/" /etc/logrotate.d/nginx
+#sed -i -e "s/create.*/create 0644 root root/" /etc/logrotate.d/nginx
+
+########################################################################################################################
+echo "Done!"
+# loop forever for debugging only
+while true; do sleep 5; done
