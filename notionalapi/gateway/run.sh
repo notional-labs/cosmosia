a083f9c8e8b33fe63d7f159e024c1c0eaa5ffd3b
Tuan Pham Anh <baabeetaa@gmail.com>
2023-08-27 12:08:41 +0700

    napidb: cluster...

--- notionalapi/gateway/run.sh
+++ notionalapi/gateway/run.sh
@@ -30,7 +30,7 @@ make install
 # config file
 cat <<EOT > $HOME/gateway.yaml
 aggrurl: "http://tasks.napiaggregator:8300/metering"
-dbconurl: "http://tasks.napidb:4001/"
+dbconurl: "http://tasks.napidb_1:4001/"
 rpc: "http://tasks.lb_${chain_name}:8000"
 api: "http://tasks.lb_${chain_name}:8001"
 grpc: "tasks.lb_${chain_name}:8003"
