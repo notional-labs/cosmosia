c9d664051df100af9806a12e51454953631ec27d
Tuan Pham Anh <baabeetaa@gmail.com>
2023-09-20 03:54:38 +0700

    napi: mysql...

--- notionalapi/mysql/run.sh
+++ notionalapi/mysql/run.sh
@@ -10,10 +10,24 @@ screen -S mysql -dm /usr/bin/mysqld --user=root --datadir='/var/lib/mysql'
 
 # init
 mysql --user=root mysql <<< cat <<EOT
-CREATE USER  'root'@'%' IDENTIFIED BY 'invalid';
+CREATE USER 'root'@'%' IDENTIFIED BY 'invalid';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 GRANT PROXY ON ''@'%' TO 'root'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
+
+CREATE DATABASE db_apicount;
+USE db_apicount;
+CREATE TABLE tbl_apicount (
+	id CHAR(32) NOT NULL,
+	dt  INT NOT NULL,
+	apikey CHAR(32) NOT NULL,
+	chain CHAR(27) NOT NULL,
+	protocol CHAR(1) NOT NULL,
+	method VARCHAR(128) NOT NULL,
+	point INT NOT NULL,
+	PRIMARY KEY (id),
+	INDEX (dt, apikey, chain, protocol)
+);
 EOT
 
 # shutdown
