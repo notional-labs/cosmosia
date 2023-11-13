827898214b398558078b72b823b4bb15e80a1de1
Tuan Pham Anh <baabeetaa@gmail.com>
2023-11-14 03:38:50 +0700

    update internal proxy

--- proxy/internal/docker_service_create.sh
+++ proxy/internal/docker_service_create.sh
@@ -10,6 +10,14 @@ docker service create \
   --publish mode=host,target=9090,published=9090 \
   --network cosmosia \
   --network subnode \
+  --network net1 \
+  --network net2 \
+  --network net3 \
+  --network net4 \
+  --network net5 \
+  --network net6 \
+  --network net7 \
+  --network net8 \
   --constraint 'node.hostname==cosmosia30' \
   --sysctl 'net.ipv4.tcp_tw_reuse=1' \
   --restart-condition none \
