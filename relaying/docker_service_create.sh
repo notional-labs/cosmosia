b01a7e39b72dbb72d3a5b6ddeafc476f279f1397
Tuan Pham Anh <baabeetaa@gmail.com>
2023-12-16 23:38:43 +0700

    ...

--- relaying/docker_service_create.sh
+++ relaying/docker_service_create.sh
@@ -1,9 +1,21 @@
+# usage: ./docker_service_create.sh hubname
+# eg., ./docker_service_create.sh whitewhale
+
+hubname="$1"
+if [[ -z $hubname ]]; then
+  echo "No hubname. usage eg., ./docker_service_create.sh whitewhale"
+  exit
+fi
+
+rl_service_name="rl_${hubname}"
+
+
 # delete existing service
-docker service rm rl_test
+docker service rm $rl_service_name
 
 # create new service
 docker service create \
-  --name rl_test \
+  --name $rl_service_name \
   --replicas 1 \
   --network bignet \
   --network net1 \
@@ -19,6 +31,6 @@ docker service create \
   --restart-condition none \
   archlinux:latest \
   /bin/bash -c \
-  "curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/491-relaying-test-relaying/relaying/run.sh > ~/run.sh && /bin/bash ~/run.sh"
+  "curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/491-relaying-test-relaying/relaying/run.sh > ~/run.sh && /bin/bash ~/run.sh $hubname"
 
 
--- relaying/run.sh
+++ relaying/run.sh
@@ -1,13 +1,36 @@
+# usage: ./run.sh hubname
+# eg., ./run.sh whitewhale
+
+# functions
+loop_forever () {
+  echo "loop forever for debugging only"
+  while true; do sleep 5; done
+}
+
+hubname="$1"
+if [[ -z $hubname ]]; then
+  echo "No hubname!"
+  loop_forever
+fi
+
+
 pacman -Syu --noconfirm
 pacman -S --noconfirm git base-devel screen wget
 
 # install hermes
 cd $HOME
 mkdir -p $HOME/.hermes/bin
-wget -O - "https://github.com/informalsystems/hermes/releases/download/v1.7.4/hermes-v1.7.4-x86_64-unknown-linux-gnu.tar.gz" |tar -xz -C $HOME/.hermes/bin
+wget -O - "https://github.com/informalsystems/hermes/releases/download/v1.7.4/hermes-v1.7.4-x86_64-unknown-linux-gnu.tar.gz" |tar -xz -C $HOME/.hermes/bin/
+
+# hermes config
+curl -Ls "https://raw.githubusercontent.com/notional-labs/cosmosia/491-relaying-test-relaying/relaying/${hubname}_config.toml" > $HOME/.hermes/config.toml
+curl -Ls "http://tasks.web_config/config/cosmosia.relay.${hubname}.mnemonic.txt" > $HOME/.hermes/mnemonic.txt
 
 
+chain_ids=$(cat $HOME/.hermes/config.toml |grep id |sed -e "s/id = //g" -e "s/'//g")
+for chain_id in $chain_ids; do
+  $HOME/.hermes/bin/hermes keys add --chain $chain_id --mnemonic-file $HOME/.hermes/mnemonic.txt
+done
 
 
-# loop forever for debugging only
-while true; do sleep 5; done
+loop_forever
