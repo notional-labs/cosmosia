33adda2abb480428ac8180c12da73f2a0b0ab623
Tuan Pham Anh <baabeetaa@gmail.com>
2023-12-16 23:49:03 +0700

    add relaying to Cosmosia (#492)

---
+++ relaying/docker_service_create.sh
@@ -0,0 +1,36 @@
+# usage: ./docker_service_create.sh hubname
+# eg., ./docker_service_create.sh whitewhale
+
+hubname="$1"
+if [[ -z $hubname ]]; then
+  echo "No hubname. usage eg., ./docker_service_create.sh whitewhale"
+  exit
+fi
+
+rl_service_name="rl_${hubname}"
+
+
+# delete existing service
+docker service rm $rl_service_name
+
+# create new service
+docker service create \
+  --name $rl_service_name \
+  --replicas 1 \
+  --network bignet \
+  --network net1 \
+  --network net2 \
+  --network net3 \
+  --network net4 \
+  --network net5 \
+  --network net6 \
+  --network net7 \
+  --network net8 \
+  --constraint 'node.hostname==cosmosia33' \
+  --endpoint-mode dnsrr \
+  --restart-condition none \
+  archlinux:latest \
+  /bin/bash -c \
+  "curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/main/relaying/run.sh > ~/run.sh && /bin/bash ~/run.sh $hubname"
+
+
