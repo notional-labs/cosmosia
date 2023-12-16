154c214e3dae23bb142101e8f9c4f2769f29cee9
Tuan Pham Anh <baabeetaa@gmail.com>
2023-12-16 10:05:19 +0700

    install hermes

--- relaying/run.sh
+++ relaying/run.sh
@@ -1,8 +1,10 @@
 pacman -Syu --noconfirm
-pacman -S --noconfirm git base-devel screen
+pacman -S --noconfirm git base-devel screen wget
 
-
-
+# install hermes
+cd $HOME
+mkdir -p $HOME/.hermes/bin
+wget -O - "https://github.com/informalsystems/hermes/releases/download/v1.7.4/hermes-v1.7.4-x86_64-unknown-linux-gnu.tar.gz" |tar -xz -C $HOME/.hermes/bin
 
 
 
