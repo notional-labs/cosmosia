7ecfd5e70ea64778c3e257c4acade6b4d7327c78
Tuan Pham Anh <baabeetaa@gmail.com>
2023-12-17 11:53:44 +0700

    relaying: use hermes verson from relayerhub_registry.ini config

--- relaying/run.sh
+++ relaying/run.sh
@@ -17,10 +17,28 @@ fi
 pacman -Syu --noconfirm
 pacman -S --noconfirm git base-devel screen wget
 
+# get hermes config
+eval "$(curl -s "https://raw.githubusercontent.com/notional-labs/cosmosia/main/relaying/relayerhubs_registry.ini" |awk -v TARGET=$hubname -F ' = ' '
+  {
+    if ($0 ~ /^\[.*\]$/) {
+      gsub(/^\[|\]$/, "", $0)
+      SECTION=$0
+    } else if (($2 != "") && (SECTION==TARGET)) {
+      print $1 "=" $2
+    }
+  }
+  ')"
+echo "hermes_version=${hermes_version}"
+if [[ -z $hermes_version ]]; then
+  echo "No hermes_version!"
+  loop_forever
+fi
+
+
 # install hermes
 cd $HOME
 mkdir -p $HOME/.hermes/bin
-wget -O - "https://github.com/informalsystems/hermes/releases/download/v1.7.4/hermes-v1.7.4-x86_64-unknown-linux-gnu.tar.gz" |tar -xz -C $HOME/.hermes/bin/
+wget -O - "https://github.com/informalsystems/hermes/releases/download/${hermes_version}/hermes-${hermes_version}-x86_64-unknown-linux-gnu.tar.gz" |tar -xz -C $HOME/.hermes/bin/
 
 # hermes config
 curl -Ls "https://raw.githubusercontent.com/notional-labs/cosmosia/main/relaying/${hubname}_config.toml" > $HOME/.hermes/config.toml
