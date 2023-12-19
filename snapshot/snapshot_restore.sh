3a7dddd27e8b7e0203a1c0ec253e57026788ff51
Tuan Pham Anh <baabeetaa@gmail.com>
2023-12-19 18:34:09 +0700

    disable discard_abci_responses

--- snapshot/snapshot_restore.sh
+++ snapshot/snapshot_restore.sh
@@ -237,7 +237,7 @@ fi
 sed -i -e "s/^query_gas_limit *=.*/query_gas_limit = 10000000/" $node_home/config/app.toml
 sed -i -e "s/^indexer *=.*/indexer = \"kv\"/" $node_home/config/config.toml
 
-[ "$chain_name" == "celestia" ] && sed -i -e "s/^discard_abci_responses *=.*/discard_abci_responses = false/" $node_home/config/config.toml
+sed -i -e "s/^discard_abci_responses *=.*/discard_abci_responses = false/" $node_home/config/config.toml
 
 echo "download genesis..."
 curl -Ls "${SNAPSHOT_BASE_URL}/genesis.json" > $node_home/config/genesis.json
