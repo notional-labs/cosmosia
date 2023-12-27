a882a4ebfa857e64c35a66634bbbdf79b245de4a
Tuan Pham Anh <baabeetaa@gmail.com>
2023-12-28 02:26:54 +0700

    fix missing account

--- relaying_clear/metrics/main.py
+++ relaying_clear/metrics/main.py
@@ -2,7 +2,18 @@ import os
 import subprocess
 import time
 import shlex
+import json
 from prometheus_client import Gauge, start_http_server
+
+
+def get_account_address(chain_id, keyname):
+    result = ""
+    dir_path = f'/root/.hermes/keys/{chain_id}/keyring-test/{keyname}.json'
+    with open(dir_path) as json_file:
+        json_obj = json.load(json_file)
+        result = json_obj["account"]
+
+    return result
 
 
 def get_subfolders_in_path(path):
@@ -15,7 +26,18 @@ def extract_result(str):
     # SUCCESS balance for key `migaloo`: 82731397 uwhale
     if str.startswith("SUCCESS"):
         subs = str.split(": ", 1)
-        res = subs[1].strip()
+        balance = subs[1].strip()
+        balance_subs = balance.split(" ", 1)
+        balance_value = int(subs[0])
+        balance_denom = subs[1]
+
+        subs = str.split("`", 1)
+        keyname = subs[1]
+        res = {
+            "keyname": keyname,
+            "balance": balance_value,
+            "denom": balance_denom,
+        }
 
     return res
 
@@ -40,7 +62,7 @@ def metrics_server():
     start_http_server(8000) # Start up the server to expose the metrics.
 
     # wallet_balance{account="migaloo1t53wkmgjp5vksed2hw4e3625kq0jyas06w9shm",chain="migaloo-1",denom="uwhale",service_name="unknown_service",otel_scope_name="hermes",otel_scope_version=""} 22086720
-    g_wallet_balance = Gauge('wallet_balance', 'TYPE wallet_balance gauge', ["chain", "denom"])
+    g_wallet_balance = Gauge('wallet_balance', 'TYPE wallet_balance gauge', ["account", "chain", "denom"])
 
     while True:
         for chain_id in chain_ids:
@@ -49,11 +71,8 @@ def metrics_server():
             txtout = run_cmd(f'/root/.hermes/bin/hermes keys balance --chain {chain_id}')
             balance = extract_result(txtout)
             if balance is not None:
-                subs = balance.split(" ", 1)
-                v = int(subs[0])
-                denom = subs[1]
-
-                g_wallet_balance.labels(chain=chain_id, denom=denom).set(v)
+                account = get_account_address(chain_id, balance["keyname"])
+                g_wallet_balance.labels(account=account, chain=chain_id, denom=balance["denom"]).set(balance["balance"])
 
         time.sleep(60)
 
