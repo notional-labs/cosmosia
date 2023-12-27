b423e102d8b8b656850836a9d7b96b0fb815c933
Tuan Pham Anh <baabeetaa@gmail.com>
2023-12-28 00:43:41 +0700

    ...

--- relaying_clear/metrics/main.py
+++ relaying_clear/metrics/main.py
@@ -1,12 +1,13 @@
 import os
 import subprocess
 import time
+import shlex
 from prometheus_client import Gauge, start_http_server
 
 
-def get_subfolders_in_path():
+def get_subfolders_in_path(path):
     # /root/.hermes/keys
-    subfolders = [f.name for f in os.scandir("/Users/tuanpa/Work/notional/cosmosia") if f.is_dir()]
+    subfolders = [f.name for f in os.scandir(path) if f.is_dir()]
     return subfolders
 
 def extract_result(str):
@@ -18,26 +19,43 @@ def extract_result(str):
 
     return res
 
-def run_cmd():
+def run_cmd(cmd):
     """
     # $HOME/.hermes/bin/hermes keys balance --chain migaloo-1
     SUCCESS balance for key `migaloo`: 82731397 uwhale
     """
-    result = subprocess.run(['ls', '-Y'], stdout=subprocess.PIPE)
-    print(result.stdout.decode('utf-8'))
+
+    args = shlex.split(cmd)
+    result = subprocess.run([args], stdout=subprocess.PIPE)
+    txtout = result.stdout.decode('utf-8')
+    print(txtout)
+    return txtout
 
 
 def metrics_server():
+    chain_ids = get_subfolders_in_path('/root/.hermes/keys')
+    print(f'chain_ids=${chain_ids}')
+
+
     # Start up the server to expose the metrics.
     start_http_server(8000)
 
     # wallet_balance{account="migaloo1t53wkmgjp5vksed2hw4e3625kq0jyas06w9shm",chain="migaloo-1",denom="uwhale",service_name="unknown_service",otel_scope_name="hermes",otel_scope_version=""} 22086720
-    g_wallet_balance = Gauge('wallet_balance', 'TYPE wallet_balance gauge', ["label1", "label2"])
+    g_wallet_balance = Gauge('wallet_balance', 'TYPE wallet_balance gauge', ["chain", "denom"])
     # g_wallet_balance1 = Gauge('wallet_balance1', 'TYPE wallet_balance gauge', ["label1", "label2"])
 
     while True:
-        g_wallet_balance.labels(label1="value1", label2="value2").set(1)
-        g_wallet_balance.labels(label1="value3", label2="value4").set(3.4)
+        for chain_id in chain_ids:
+            print(f'processing ${chain_id}')
+
+            txtout = run_cmd(f'/root/.hermes/bin/hermes keys balance --chain {chain_id}')
+            balance = extract_result(txtout)
+            subs = balance.split(" ", 1)
+            v = int(subs[0])
+            denom = subs[1]
+
+            g_wallet_balance.labels(chain=chain_id, denom=denom).set(v)
+
         time.sleep(60)
 
 
@@ -45,4 +63,4 @@ if __name__ == '__main__':
     # metrics_server()
     # run_cmd()
     # print(extract_result("SUCCESS balance for key `migaloo`: 82731397 uwhale"))
-    get_subfolders_in_path()
+
