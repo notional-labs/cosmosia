b05bd1b55f6a037034165f1b705cc48e3f6211e2
Tuan Pham Anh <baabeetaa@gmail.com>
2023-12-29 17:23:56 +0700

    loki...

---
+++ monitor/loki/docker_service_create.sh
@@ -0,0 +1,16 @@
+# delete existing service
+docker service rm loki
+
+# create new service
+docker service create \
+  --name loki \
+  --replicas 1 \
+  --network bignet \
+  --constraint "node.labels.cosmosia.relay==true" \
+  --endpoint-mode dnsrr \
+  --restart-condition any \
+  archlinux:latest \
+  /bin/bash -c \
+  "curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/loki/monitor/main/run.sh > ~/run.sh && /bin/bash ~/run.sh"
+
+
---
+++ monitor/loki/run.sh
@@ -0,0 +1,11 @@
+pacman -Syu --noconfirm
+pacman -S --noconfirm screen
+
+cd $HOME
+
+
+
+########################################################################################################################
+echo "Done!"
+# loop forever for debugging
+while true; do sleep 5; done
