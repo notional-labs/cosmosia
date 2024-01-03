87962099781700b30772bc354d2fdf8fad0d3b17
Tuan Pham Anh <baabeetaa@gmail.com>
2024-01-03 09:45:32 +0700

    val: disable priv_validator_laddr when spinning new node

--- validator/run.sh
+++ validator/run.sh
@@ -68,8 +68,8 @@ cd $HOME
 source $HOME/snapshot_restore.sh
 
 # for remote signer
-# sed -i -e 's/^priv_validator_laddr *=.*/priv_validator_laddr = ""/' $node_home/config/config.toml
-sed -i -e 's/^priv_validator_laddr *=.*/priv_validator_laddr = "tcp:\/\/0.0.0.0:23859"/' $node_home/config/config.toml
+sed -i -e 's/^priv_validator_laddr *=.*/priv_validator_laddr = ""/' $node_home/config/config.toml
+# sed -i -e 's/^priv_validator_laddr *=.*/priv_validator_laddr = "tcp:\/\/0.0.0.0:23859"/' $node_home/config/config.toml
 
 # disable statesync
 sed -i -e "s/^snapshot-interval *=.*/snapshot-interval = 0/" $node_home/config/app.toml
