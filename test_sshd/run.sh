3f721eb418bcf44ee59fc4a67d6f9c894bff5bfd
Tuan Pham Anh <baabeetaa@gmail.com>
2024-02-25 16:06:56 +0700

    ...

--- test_sshd/run.sh
+++ test_sshd/run.sh
@@ -1,4 +1,15 @@
 cd $HOME
+
+# functions
+loop_forever () {
+  echo "loop forever for debugging only"
+  while true; do sleep 5; done
+}
+
 pacman -Syu --noconfirm
 pacman -Sy --noconfirm base-devel wget jq dnsutils inetutils openssh
 pacman -Syu --noconfirm
+
+
+
+loop_forever
