612ed1fb6efac12ca62e44c8993038ddc4b3cc04
Tuan Pham Anh <baabeetaa@gmail.com>
2024-03-31 21:49:32 +0700

    ...

--- notionalapi/db_backup/docker_service_create.sh
+++ notionalapi/db_backup/docker_service_create.sh
@@ -15,4 +15,4 @@ docker service create \
   --mount type=bind,source=/mnt/data/napi_dbbackup,destination=/data \
   archlinux:latest \
   /bin/bash -c \
-  "curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/583-add-a-service-to-backup-notionalapi-db-daily/notionalapi/db_backup/run.sh > ~/run.sh && /bin/bash ~/run.sh"
+  "curl -s https://raw.githubusercontent.com/notional-labs/cosmosia/main/notionalapi/db_backup/run.sh > ~/run.sh && /bin/bash ~/run.sh"
