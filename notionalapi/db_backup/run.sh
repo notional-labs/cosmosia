2ce6d31390594dc73de38f37def86fd134eda9d6
Tuan Pham Anh <baabeetaa@gmail.com>
2024-03-31 21:58:09 +0700

    Update notionalapi/db_backup/run.sh
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- notionalapi/db_backup/run.sh
+++ notionalapi/db_backup/run.sh
@@ -15,7 +15,7 @@ cd "$HOME" || exit
 
 #########
 # setup cronjob
-curl -Ls "https://raw.githubusercontent.com/notional-labs/cosmosia/main/notionalapi/db_backup/backup.sh" > $HOME/backup.sh
+curl -Ls "https://raw.githubusercontent.com/notional-labs/cosmosia/main/notionalapi/db_backup/backup.sh" > "$HOME"/backup.sh
 
 echo "0 */12 * * * root /bin/bash $HOME/backup.sh" > /etc/cron.d/cron_backup
 
