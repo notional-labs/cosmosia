d6a2db9a29c8d05c17308a63518e5dbdb4344705
Tuan Pham Anh <baabeetaa@gmail.com>
2024-04-14 08:21:18 +0700

    adding osmosis-archive-sub3...

---
+++ data/custom_build_scripts/osmosis-archive-sub3.sh
@@ -0,0 +1,21 @@
+cd $HOME
+
+if [[ -z $upgrading ]]; then
+  git clone --single-branch --branch $version $git_repo
+  repo_name=$(basename $git_repo |cut -d. -f1)
+  cd $repo_name
+else
+  repo_name=$(basename $git_repo |cut -d. -f1)
+  cd $repo_name
+  git reset --hard
+  git fetch --all --tags
+  git checkout "$p_version"
+fi
+
+go work use
+
+go mod edit -replace github.com/tendermint/tm-db=github.com/notional-labs/tm-db@v0.11.0
+go mod tidy
+go mod edit -replace github.com/cometbft/cometbft-db=github.com/notional-labs/cometbft-db@pebble
+go mod tidy
+GOWORK=off go install -tags pebbledb -ldflags "-w -s -X github.com/cosmos/cosmos-sdk/types.DBBackend=pebbledb $opt_forcesync" ./...
