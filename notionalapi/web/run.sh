6f3294143dee9b103b8e49b590e09b71a2493e6b
Tuan Pham Anh <baabeetaa@gmail.com>
2024-05-23 11:27:46 +0700

    napi: use main branch

--- notionalapi/web/run.sh
+++ notionalapi/web/run.sh
@@ -13,7 +13,7 @@ yarn set version v18.14.0
 
 cd $HOME
 gh_access_token="$(curl -s "http://tasks.web_config/config/gh_access_token")"
-git clone --single-branch --branch v0.3.x "https://${gh_access_token}@github.com/notional-labs/notionalapi"
+git clone --single-branch --branch main "https://${gh_access_token}@github.com/notional-labs/notionalapi"
 
 ########################################################################################################################
 # web
