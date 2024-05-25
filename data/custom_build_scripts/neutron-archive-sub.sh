6e95b956b3795bf88faed72a17cd043f3061e1f4
Tuan Pham Anh <baabeetaa@gmail.com>
2024-05-25 11:12:27 +0700

    upgrade: neutron-archive-sub v3.0.5

--- data/custom_build_scripts/neutron-archive-sub.sh
+++ data/custom_build_scripts/neutron-archive-sub.sh
@@ -16,6 +16,8 @@ go mod edit -replace github.com/tendermint/tm-db=github.com/notional-labs/tm-db@
 go mod tidy
 go mod edit -replace github.com/cometbft/cometbft-db=github.com/notional-labs/cometbft-db@pebble
 go mod tidy
+go mod edit -replace github.com/cosmos/cosmos-db=github.com/notional-labs/cosmos-db@v1.0.0-139b9ba
+go mod tidy
 go mod edit -replace github.com/cockroachdb/pebble=github.com/cockroachdb/pebble@v1.0.0
 go mod tidy
 go install -tags pebbledb -ldflags "-w -s -X github.com/cosmos/cosmos-sdk/types.DBBackend=pebbledb" ./...
