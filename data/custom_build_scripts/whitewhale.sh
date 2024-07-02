b0ee0b931653cc5ec4f1f047e9275cb0b7c37fb7
Tuan Pham Anh <baabeetaa@gmail.com>
2024-07-02 11:47:59 +0700

    fix whitewhale

--- data/custom_build_scripts/whitewhale.sh
+++ data/custom_build_scripts/whitewhale.sh
@@ -15,6 +15,7 @@ cd $HOME
 # TODO: fix building temporary, got apphash with release/v4.1.x branch, only commit f5b51641c07e7637dc1a9d38477c2a543bcf47aa works
 cd $HOME
 git clone https://github.com/White-Whale-Defi-Platform/migaloo-chain
+cd migaloo-chain
 git checkout f5b51641c07e7637dc1a9d38477c2a543bcf47aa
 
 go install -tags pebbledb -ldflags "-w -s -X github.com/cosmos/cosmos-sdk/types.DBBackend=pebbledb" ./...
