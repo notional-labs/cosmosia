cbee170398a47d6238d92a24128d6fbfcaa542a7
Tuan Pham Anh <baabeetaa@gmail.com>
2024-07-06 14:07:54 +0700

    relay: add rly

--- relay/rly/run.sh
+++ relay/rly/run.sh
@@ -18,30 +18,26 @@ pacman -Syu --noconfirm
 pacman -S --noconfirm git base-devel python python-pip cronie screen wget jq
 
 ################################################################################################
-# install rly
+# install
 cd $HOME
 wget "https://github.com/cosmos/relayer/releases/download/v2.5.2/Cosmos.Relayer_2.5.2_linux_amd64.tar.gz"
-tar -xz Cosmos.Relayer_2.5.2_linux_amd64.tar.gz -C $HOME/
+tar -xf Cosmos.Relayer_2.5.2_linux_amd64.tar.gz -C $HOME/
+mv 'Cosmos Relayer_2.5.2_linux_amd64'/rly /usr/bin/
 rm -rf 'Cosmos Relayer_2.5.2_linux_amd64'/
 
-# rly config
+################################################################################################
+# config
 cd $HOME
 mkdir $HOME/.relayer/config
-# /.relayer/config/config.yaml
+curl -Ls "https://raw.githubusercontent.com/notional-labs/cosmosia/main/relay/rly/config/${hubname}_config.yaml" > $HOME/.relayer/config/config.yaml
 curl -Ls "http://tasks.web_config/config/cosmosia.relay_clear.${hubname}.mnemonic.txt" > $HOME/.relayer/config/mnemonic.txt
 MNEMONIC=$(cat $HOME/.relayer/config/mnemonic.txt)
 export MNEMONIC="$MNEMONIC"
+rly keys restore default "${MNEMONIC}" --restore-all
 
-# TODO: use https://kislyuk.github.io/yq/ to get list of chains
-#chains=$(cat $HOME/.relayer/config/config.yaml |grep "id = " |sed -e "s/id = //g" -e "s/'//g" -e 's/"//g')
-#for chain in $chain_ids; do
-#  rly keys restore composable default "${MNEMONIC}"
-#done
-
-
-
-
-
+################################################################################################
+# run
+screen -S rly -dm rly start
 
 ################################################################################################
 loop_forever
