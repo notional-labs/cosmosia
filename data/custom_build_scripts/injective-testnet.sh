7e2eff20623b477eb2b93fc157e031b5a052a409
Tuan Pham Anh <baabeetaa@gmail.com>
2024-07-19 23:15:31 +0700

    upgrade: injective-testnet v1.13.2-1721343631

--- data/custom_build_scripts/injective-testnet.sh
+++ data/custom_build_scripts/injective-testnet.sh
@@ -8,7 +8,7 @@ if [[ -z $upgrading ]]; then
   mv libwasmvm.x86_64.so /usr/lib
   rm linux-amd64.zip
 else
-  wget "https://github.com/InjectiveLabs/testnet/releases/download/${$p_version}/linux-amd64.zip"
+  wget "https://github.com/InjectiveLabs/testnet/releases/download/${p_version}/linux-amd64.zip"
   unzip linux-amd64.zip
   mv injectived peggo $HOME/go/bin
   mv libwasmvm.x86_64.so /usr/lib
